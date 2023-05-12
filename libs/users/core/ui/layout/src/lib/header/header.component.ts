afae759d1e87f7cbaaeb3e34b4c26edf1424303e
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-12 12:22:52 +0600

    feat(layout) generate header component

---
+++ libs/users/core/ui/layout/src/lib/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
