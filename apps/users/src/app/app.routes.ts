5def568975d97a62c823c346e1ceaad70e520040
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-12 13:09:46 +0600

    feat(users) home component

--- apps/users/src/app/app.routes.ts
+++ apps/users/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: 'home',
-  //   // loadComponent: () => import()
-  // }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('@users/home').then(c => c.HomeComponent)
+  }
 ];
---
+++ libs/users/home/src/lib/home/home.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'users-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
