246c2baae784936665645d7be81cea3492bf5164
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-15 12:53:38 +0600

    feat(core-http) implements api service

--- apps/users/src/app/app.config.ts
+++ apps/users/src/app/app.config.ts
@@ -5,12 +5,18 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import { API_URL } from '@users/core/http';
+import { environment } from '../environments/environment.development'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     importProvidersFrom(
-    )
+    ),
+    {
+      provide: API_URL,
+      useValue: environment.api_url
+    }
   ],
 };
---
+++ apps/users/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  api_url: 'https://jsonplaceholder.typicode.com'
+};
