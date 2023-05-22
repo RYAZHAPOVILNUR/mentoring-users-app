e98ae7dddcf9d7862ab3bfa7f6cce2c0f1df444b
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-22 12:47:24 +0600

    fix(users app) remove ngrx

--- apps/users/src/app/app.config.ts
+++ apps/users/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -7,19 +7,9 @@ import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { API_URL } from '@users/core/http';
 import { environment } from '../environments/environment.development';
-import { provideStore, provideState } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import * as fromUsers from './+state/users.reducer';
-import { UsersEffects } from './+state/users.effects';
-import { UsersFacade } from './+state/users.facade';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideEffects(UsersEffects),
-    provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
-    provideEffects(),
-    provideStore(),
-    UsersFacade,
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     {
