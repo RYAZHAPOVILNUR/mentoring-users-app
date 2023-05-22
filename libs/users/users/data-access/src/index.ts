95d3114183752fee67ef55d5000be7ec3896ea28
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-22 13:01:56 +0600

    feat(user-data-access) generate ngrx users feature

---
+++ libs/users/users/data-access/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/+state/users.facade';
+export * from './lib/+state/users.models';
+export * from './lib/+state/users.selectors';
+export * from './lib/+state/users.reducer';
+export * from './lib/+state/users.actions';
+export * from './lib/users-data-access.module';
