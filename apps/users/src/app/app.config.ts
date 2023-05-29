bef66ffc90f1e6d4dcfb62c51e001107a50792a5
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-29 12:28:00 +0600

    chore(users app) install angular material

--- apps/users/src/app/app.config.ts
+++ apps/users/src/app/app.config.ts
@@ -10,7 +10,8 @@ import { environment } from '../environments/environment.development';
 import { provideEffects } from '@ngrx/effects';
 import { provideState, provideStore } from '@ngrx/store';
 import { USERS_FEATURE_KEY, UsersFacade, usersReducer, userEffects } from '@users/users/data-access';
-import { provideStoreDevtools } from '@ngrx/store-devtools'
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideNoopAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,17 +20,18 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     UsersFacade,
     provideStoreDevtools({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: false,
+        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
     }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     {
-      provide: API_URL,
-      useValue: environment.api_url,
+        provide: API_URL,
+        useValue: environment.api_url,
     },
-  ],
+    provideNoopAnimations()
+],
 };
