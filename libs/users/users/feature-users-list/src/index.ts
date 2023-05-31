b6364b677b3e7163b686bd716e74d9dda5cf82fe
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-31 14:03:20 +0600

    feat(users) add create users dialog

--- libs/users/users/feature-users-list/src/index.ts
+++ libs/users/users/feature-users-list/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/users-list-container/users-list-container.component';
 export * from './lib/users-list/users-list.component';
-export * from './lib/users-vm';
+export * from '../../users-vm';
--- libs/users/users/feature-users-list/src/lib/users-list/users-list.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-list/users-list.component.ts
@@ -13,7 +13,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { UsersCardComponent } from "../users-card/users-card.component";
-import { UsersVM } from '../users-vm';
+import { UsersVM } from '../../../../users-vm';
 
 @Component({
   selector: 'users-list-ui',
