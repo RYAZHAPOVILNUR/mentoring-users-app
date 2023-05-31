7be5775e6c902bb8a6c66d3705a4d1970bc6b423
Rushannn <rushankarimov@gmail.com>
2023-05-31 18:40:27 +0300

    feat (users) complete create users dialog

--- libs/users/users/feature-users-create/src/lib/create-users-button/create-users-button.component.ts
+++ libs/users/users/feature-users-create/src/lib/create-users-button/create-users-button.component.ts
@@ -1,15 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { CreateUsersDialogComponent } from '../create-users-dialog/create-users-dialog.component';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'create-users-button',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './create-users-button.component.html',
   styleUrls: ['./create-users-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreateUsersButtonComponent {}
+export class CreateUsersButtonComponent {
+  private name!: string;
+  private email!: string;
+
+
+  public dialog = inject(MatDialog);
+
+  openAddUserDialog(): void {
+    const dialogRef: MatDialogRef<CreateUsersDialogComponent> = this.dialog.open(CreateUsersDialogComponent, {
+      data: { name: this.name, email: this.email },
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      console.log('The dialog was closed', data);
+    });
+  }
+}
--- libs/users/users/feature-users-create/src/lib/create-users-dialog/create-users-dialog.component.ts
+++ libs/users/users/feature-users-create/src/lib/create-users-dialog/create-users-dialog.component.ts
@@ -1,12 +1,51 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'users-create-users-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './create-users-dialog.component.html',
   styleUrls: ['./create-users-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreateUsersDialogComponent {}
+export class CreateUsersDialogComponent {
+
+  public formGroup: FormGroup;
+  private formBuilder = inject(FormBuilder);
+  public dialogRef = inject(MatDialogRef<CreateUsersDialogComponent>);
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { name: string, email: string },
+
+  ) {
+    this.formGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    if (this.formGroup.valid) {
+      const formData = this.formGroup.value;
+      this.dialogRef.close(formData);
+    }
+  }
+}
