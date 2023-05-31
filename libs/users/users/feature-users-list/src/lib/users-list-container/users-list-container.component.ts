e5016bb02e94c6a7deb9d49d8dc3154ec00d165a
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-05-31 12:44:14 +0600

    refactor(users) move open dialog to container store

--- libs/users/users/feature-users-list/src/lib/users-list-container/users-list-container.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-list-container/users-list-container.component.ts
@@ -9,8 +9,7 @@ import { UsersListComponent } from '../users-list/users-list.component';
 import { UsersListContainerStore } from './users-list-container.store';
 import { UsersVM } from '../users-vm';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { UsersUserDeleteDialogComponent } from '../users-user-delete-dialog/users-user-delete-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { UsersFacade } from '@users/users/data-access';
 
 @Component({
@@ -27,22 +26,9 @@ export class UsersListContainerComponent {
   private readonly componentStore = inject(UsersListContainerStore);
   public readonly users$ = this.componentStore.users$;
   public readonly status$ = this.componentStore.status$;
-  public dialog = inject(MatDialog);
   public usersFacade = inject(UsersFacade);
 
   onDeleteUser(user: UsersVM) {
-    const dialogRef: MatDialogRef<UsersUserDeleteDialogComponent> = this.dialog.open(UsersUserDeleteDialogComponent,{
-      data: {name: user.name}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === true) {
-        this.usersFacade.deleteUser(user.id)
-      } else {
-        // Обработка события "Нет"
-      }
-    });
+    this.componentStore.deleteUser(user)
   }
-
-
 }
