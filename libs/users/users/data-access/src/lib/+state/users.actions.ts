68d03387e508e06cc333a2eeeca4ef72a464d67a
Rushannn <rushankarimov@gmail.com>
2023-06-01 10:36:51 +0300

    refactor (users) change action types

--- libs/users/users/data-access/src/lib/+state/users.actions.ts
+++ libs/users/users/data-access/src/lib/+state/users.actions.ts
@@ -16,9 +16,9 @@ export const loadUsersFailure = createAction(
 
 export const deleteUser = createAction('[Users Page] Delete User', props<{id:number}>());
 export const deleteUserSuccess = createAction('[Users/Api] Delete User Success', props<{id:number}>());
-export const deleteUserFailed = createAction('[Users/Api] Delete User Success', props<{ error: any }>());
+export const deleteUserFailed = createAction('[Users/Api] Delete User Failed', props<{ error: any }>());
 
 
 export const addUser = createAction('[Users Page] Add User', props<{userData:UsersEntity}>());
 export const addUserSuccess = createAction('[Users/Api] Add User Success', props<{userData:UsersEntity}>());
-export const addUserFailed = createAction('[Users/Api] Add User Success', props<{ error: any }>());
+export const addUserFailed = createAction('[Users/Api] Add User Failed', props<{ error: any }>());
