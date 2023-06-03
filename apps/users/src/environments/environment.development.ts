c84df6a60cdb16a0e174480409bbb7a86b7efd61
Rushannn <rushankarimov@gmail.com>
2023-06-04 00:32:17 +0300

    refactor (users): save changes in detail users card

--- apps/users/src/environments/environment.development.ts
+++ apps/users/src/environments/environment.development.ts
@@ -1,8 +1,3 @@
-// export const environment = {
-//   api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:mTCa14Rz'
-// };
-
-
 export const environment = {
-  api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:8QI8-k1T'
+  api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:mTCa14Rz'
 };
