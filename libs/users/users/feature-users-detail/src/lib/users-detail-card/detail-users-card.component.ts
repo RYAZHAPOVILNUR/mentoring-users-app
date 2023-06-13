b432df746579acfc7c68e6a06081676187752b23
Rushannn <rushankarimov@gmail.com>
2023-06-13 21:26:28 +0300

    feat(feature-users-detail): add logic to user-card

--- libs/users/users/feature-users-detail/src/lib/users-detail-card/detail-users-card.component.ts
+++ libs/users/users/feature-users-detail/src/lib/users-detail-card/detail-users-card.component.ts
@@ -1,14 +1,14 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {CreateUserDTO, UsersErrors} from '@users/users/data-access';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {DetailUsersCardVm} from './detail-users-card-vm';
-import {Subject} from "rxjs";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateUserDTO, UsersErrors } from '@users/users/data-access';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { DetailUsersCardVm } from './detail-users-card-vm';
+import { Subject } from "rxjs";
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -29,11 +29,11 @@ import {Subject} from "rxjs";
 })
 
 export class DetailUsersCardComponent {
-  private _vm: DetailUsersCardVm = {editMode: false, user: null, status: 'init'};
+  private _vm: DetailUsersCardVm = { editMode: false, user: null, status: 'init' };
   public get vm() {
     return this._vm;
   }
-  @Input({required: true})
+  @Input({ required: true })
   set vm(vm: DetailUsersCardVm) {
     this._vm = vm;
 
@@ -52,7 +52,7 @@ export class DetailUsersCardComponent {
       this.formGroup.disable();
     }
   }
-  @Input({required: true})
+  @Input({ required: true })
   public errors!: UsersErrors | null;
   public alertTrigger = new Subject<boolean>();
 
@@ -68,6 +68,7 @@ export class DetailUsersCardComponent {
   @Output() editUser = new EventEmitter<CreateUserDTO>();
   @Output() closeUser = new EventEmitter();
   @Output() closeEditMode = new EventEmitter();
+  @Output() openEditMode = new EventEmitter();
 
   onEditUser(userData: CreateUserDTO) {
     this.editUser.emit(userData);
@@ -92,6 +93,10 @@ export class DetailUsersCardComponent {
 
   onCloseEditMode() {
     this.closeEditMode.emit();
+  }
+
+  onOpenEditMode() {
+    this.openEditMode.emit();
   }
 
 }
