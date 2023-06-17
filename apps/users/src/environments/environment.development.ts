1ae57ad946644c89d1de71ecf54ed8c59261df50
Rushannn <rushankarimov@gmail.com>
2023-06-17 20:25:26 +0300

    chore(users): change api_url

--- apps/users/src/environments/environment.development.ts
+++ apps/users/src/environments/environment.development.ts
@@ -2,6 +2,10 @@
 //   api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:mTCa14Rz'
 // };
 
+// export const environment = {
+//   api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:8QI8-k1T'
+// };
+
 export const environment = {
-  api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:8QI8-k1T'
-};
+  api_url: 'https://x8ki-letl-twmt.n7.xano.io/api:RaqAbOVN'
+};    // Api with auth and without 'createdAt'
