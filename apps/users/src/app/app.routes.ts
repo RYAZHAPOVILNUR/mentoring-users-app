f1e151395292894c074249988b149881274fa2a7
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-07-10 15:47:32 +0600

    chore(users) update tasks.md

--- apps/users/src/app/app.routes.ts
+++ apps/users/src/app/app.routes.ts
@@ -8,7 +8,7 @@ const layoutAgnosticComponents = [
     path: 'home',
     loadComponent: () => import('@users/home').then(c => c.HomeComponent)
   }
-]
+];
 
 export const appRoutes: Route[] = [
   {
@@ -53,47 +53,3 @@ export const appRoutes: Route[] = [
     loadComponent: () => import('@auth/feature-register').then(c => c.RegisterContainerComponent)
   },
 ];
-
-
-
-
-// import { Route } from '@angular/router';
-// import { authGuard, adminGuard } from '@auth/data-access';
-
-// export const appRoutes: Route[] = [
-//   {
-//     path: '',
-//     redirectTo: 'login',
-//     pathMatch: 'full',
-//   },
-//   {
-//     path: 'home',
-//     loadComponent: () => import('@users/home').then(c => c.HomeComponent)
-//   },
-//   {
-//     path: 'admin',
-//     canActivateChild: [authGuard, adminGuard],
-//     children: [
-//       {
-//         path: 'users',
-//         loadComponent: () => import('@users/feature-users-list').then(c => c.UsersListContainerComponent)
-//       },
-//       {
-//         path: 'users/:id',
-//         loadComponent: () => import('@users/feature-users-detail').then(c => c.UsersDetailComponent),
-//       },
-//     ],
-//   },
-//   {
-//     path: 'profile',
-//     loadComponent: () => import('@users/users/profile/feature-profile').then(c => c.ProfileContainerComponent)
-//   },
-//   {
-//     path: 'login',
-//     loadComponent: () => import('@auth/feature-login').then(c => c.LoginContainerComponent)
-//   },
-//   {
-//     path: 'signup',
-//     loadComponent: () => import('@auth/feature-register').then(c => c.RegisterContainerComponent)
-//   }
-// ];
