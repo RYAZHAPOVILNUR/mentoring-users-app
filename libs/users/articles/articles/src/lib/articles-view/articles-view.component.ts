8bfb255d3f37e2149ae74bccecae1c4d93dacb95
ryazhapov-ilnur <ilnur_abu_yahya@mail.ru>
2023-07-22 14:36:57 +0600

    show articles

--- libs/users/articles/articles/src/lib/articles-view/articles-view.component.ts
+++ libs/users/articles/articles/src/lib/articles-view/articles-view.component.ts
@@ -1,20 +1,16 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Article } from '@users/users/articles/data-access';
+import {QuillModule} from "ngx-quill";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'users-articles-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, QuillModule, MatCardModule],
   templateUrl: './articles-view.component.html',
   styleUrls: ['./articles-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArticlesViewComponent {
-
   @Input({ required: true }) articles!: any
-
-
-
-
 }
