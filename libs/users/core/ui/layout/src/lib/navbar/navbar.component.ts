ffc94eb7ee147c3c5471a6c0eedd69d0daeb438c
ONYXIY <onxvnezakona@gmail.com>
2023-07-26 13:18:24 +0300

    Change btn location (Смена пароля)

--- libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
+++ libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
@@ -1,11 +1,8 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { PasswordChangeDialogComponent } from '../../../../../../../core/ui/src';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { AuthFacade, ChangePasswordPayload } from '../../../../../../../core/auth/data-access/src';
+import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -23,23 +20,4 @@ import { RouterModule } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-  private readonly dialog = inject(MatDialog);
-  private readonly destroyRef = inject(DestroyRef);
-  private readonly authFacade = inject(AuthFacade);
-
-  onOpenChangePassword() {
-    const dialogRef = this.dialog.open(PasswordChangeDialogComponent)
-    dialogRef.afterClosed()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(result => {
-        if (result) {
-          const changePasswordPayload:ChangePasswordPayload = {
-            newPassword: result.value.newPassword,
-            oldPassword: result.value.oldPassword
-          }
-          console.log(changePasswordPayload)
-          this.authFacade.changePassword(changePasswordPayload);
-        }
-      });
-  }
 }
