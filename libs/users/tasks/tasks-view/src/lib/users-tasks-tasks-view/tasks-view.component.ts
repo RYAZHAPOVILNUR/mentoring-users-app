f791a90d0694dadcca7e59f8dbb37b96ca542e13
ONYXIY <onxvnezakona@gmail.com>
2023-08-06 23:39:02 +0300

    1

--- libs/users/tasks/tasks-view/src/lib/users-tasks-tasks-view/tasks-view.component.ts
+++ libs/users/tasks/tasks-view/src/lib/users-tasks-tasks-view/tasks-view.component.ts
@@ -1,15 +1,41 @@
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'users-tasks-view',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CdkDrag, CdkDropList, NgFor],
   templateUrl: './tasks-view.component.html',
   styleUrls: ['./tasks-view.component.scss'],
 })
-export class TasksViewComponent {
-  @Input({required: true}) loggedUserId!: number;
+export class TasksViewComponent {BoardName = [
+  'IDEAS',
+  'RESEARCH',
+  'TODO',
+  'DONE',
+];
+tasks = [{
+  id: 1,
+  created_at: 'now',
+  email: 'admin@admin',
+  authorId: 0,
+  borders: {
+    boardName: 'IDEAS',
+    tasks: [{
+      taskName: 'task 1'
+    }]
+  }
+},
+{
+
+}]
+
+drop(event: CdkDragDrop<string[]>) {
+  moveItemInArray(this.BoardName, event.previousIndex, event.currentIndex);
+}
+drop1(event: CdkDragDrop<unknown>) {
+  moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+}
+
 }
