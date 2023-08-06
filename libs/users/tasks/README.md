7416b840d4e78f8f3bd969b2a2e413dd09520957
ONYXIY <onxvnezakona@gmail.com>
2023-08-06 05:17:07 +0300

    added tasks lib

---
+++ libs/users/tasks/README.md
@@ -0,0 +1,7 @@
+# users-tasks
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test users-tasks` to execute the unit tests.
---
+++ libs/users/tasks/src/lib/users-tasks/users-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'users-users-tasks',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users-tasks.component.html',
+  styleUrls: ['./users-tasks.component.css'],
+})
+export class UsersTasksComponent {}
