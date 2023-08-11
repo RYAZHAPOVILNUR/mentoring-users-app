4d8073e367d500e042be019c26279ad16bb7a41f
Rushannn <rushankarimov@gmail.com>
2023-08-11 11:00:58 +0300

    feat(users): move menu frome header

--- libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
+++ libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
+import { AuthFacade } from '@auth/data-access';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'navbar',
@@ -20,4 +22,11 @@ import { RouterModule } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  private readonly facade = inject(AuthFacade)
+  public readonly userPhoto: Observable<string | undefined> = this.facade.user$.pipe(map(user => user.photo?.url))
+  public readonly photo = this.userPhoto ? this.userPhoto : ''
+
+  public onLogout() {
+    this.facade.logout()
+  }
 }
