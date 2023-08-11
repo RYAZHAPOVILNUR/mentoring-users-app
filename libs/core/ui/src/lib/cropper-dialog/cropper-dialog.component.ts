8d813110353ddeeedfe61aa025e5c019ed1ac806
Rushannn <rushankarimov@gmail.com>
2023-08-11 09:16:20 +0300

    feat(users): complete change/add image feature

--- libs/core/ui/src/lib/cropper-dialog/cropper-dialog.component.ts
+++ libs/core/ui/src/lib/cropper-dialog/cropper-dialog.component.ts
@@ -1,49 +1,43 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CropperSettings, ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DomSanitizer } from '@angular/platform-browser';
+import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'users-cropper-dialog',
   standalone: true,
   imports: [
     CommonModule,
-    ImageCropperModule
+    ImageCropperModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './cropper-dialog.component.html',
   styleUrls: ['./cropper-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CropperDialogComponent {
-  imageBase64 = '';
-  cropImgPreview: any = '';
-  sanitizer = inject(DomSanitizer)
 
-  cropperSettings: CropperSettings;
-
-  public data: any = inject(MAT_DIALOG_DATA)
-
-  constructor() {
-    this.cropperSettings = new CropperSettings();
-    this.imageBase64 = this.data.image.src
-  }
+  public croppedImg: any = '';
+  private readonly dialogRef = inject(MatDialogRef<CropperDialogComponent>);
+  public readonly data: any = inject(MAT_DIALOG_DATA);
+  public readonly imageBase64 = this.data.image.src;
 
   cropImg(e: ImageCroppedEvent) {
-    this.cropImgPreview = e.objectUrl;
-  }
-  imgLoad() {
-    // display cropper tool
-  }
-  initCropper() {
-    // init cropper
-  }
-
-  imgFailed() {
-    // error msg
+    console.log(e)
+    if (e.base64) {
+      this.croppedImg = e.base64;
+    }
   }
 
   onCrop() {
-    console.log('cropped')
+    const croppedImageData = this.croppedImg
+    this.dialogRef.close(croppedImageData)
+  }
+
+  onClose() {
+    this.dialogRef.close(false)
   }
 }
