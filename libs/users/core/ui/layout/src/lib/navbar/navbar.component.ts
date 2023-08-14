37b04fffa9da04db3e28a53cbd1813abd2c35048
pteechkin <errorbird9001@gmail.com>
2023-08-14 21:11:03 +0800

    changed ui and added dark mode

--- libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
+++ libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
+    MatListModule,
     RouterModule
   ],
   templateUrl: './navbar.component.html',
