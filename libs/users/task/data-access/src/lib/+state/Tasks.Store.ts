1f3756f1e379528c98845e88a402a38d6bfe2ec9
ONYXIY <onxvnezakona@gmail.com>
2023-08-14 17:44:34 +0300

    added component tasks store

--- libs/users/task/data-access/src/lib/+state/Tasks.Store.ts
+++ libs/users/task/data-access/src/lib/+state/Tasks.Store.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
-import { ITask } from '../model/tasks.interface';
+import { IColumn, ITask } from '../model/tasks.interface';
+import { exhaustMap, tap } from 'rxjs';
 
 export interface TasksComponentState {
   isLoading: boolean;
@@ -8,17 +9,32 @@ export interface TasksComponentState {
   tasks: ITask[];
 }
 
+
 @Injectable()
 export class TasksStore extends ComponentStore<TasksComponentState> {
   private isLoading$ = this.select((state) => state.isLoading);
   private columnName$ = this.select((state) => state.columnName);
   private tasks$ = this.select((state) => state.tasks);
 
-  vm$ = this.select({
+  public readonly vm$ = this.select({
     isLoading: this.isLoading$,
     columnName: this.columnName$,
     tasks: this.tasks$,
   });
+
+
+  public setIsLoading = this.updater(state => ({...state, isLoading: true}));
+
+  // getColumnTasks = this.effect((trigger$) =>{
+  //   return trigger$.pipe(
+  //     tap(() => {
+  //       this.setIsLoading()
+  //     }),
+  //     exhaustMap(()=>{
+  //       return 
+  //     })
+  //   )
+  // })
 
   constructor() {
     super({
