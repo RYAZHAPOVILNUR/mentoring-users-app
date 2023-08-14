0aabf15b78004e04c08d65ba11e9bb183d0a3ab7
pteechkin <errorbird9001@gmail.com>
2023-08-14 21:11:03 +0800

    changed ui and added dark mode

--- libs/users/core/ui/layout/src/lib/header/header.component.ts
+++ libs/users/core/ui/layout/src/lib/header/header.component.ts
@@ -2,11 +2,13 @@ import {Component, OnInit, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import {AuthFacade} from "@auth/data-access";
 import {map, Observable} from "rxjs";
 import {MatMenuModule} from "@angular/material/menu";
 import { PushPipe } from '@ngrx/component';
+import { ThemeSwitchService } from '@users/users/core/ui/theme-switch';
 
 
 
@@ -14,15 +16,25 @@ import { PushPipe } from '@ngrx/component';
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, RouterModule, MatMenuModule, PushPipe],
+  imports: [
+    CommonModule, 
+    MatButtonModule, 
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule, 
+    MatMenuModule, 
+    PushPipe
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit{
-  facade = inject(AuthFacade)
-  isAuthenticated$: Observable<boolean> = this.facade.isAuthenticated$
-  username$: Observable<string> = this.facade.user$.pipe(map(user => user.name))
-  isAdmin$: Observable<boolean | null> = this.facade.isAdmin$
+  private readonly facade = inject(AuthFacade)
+  private readonly themeSwitch = inject(ThemeSwitchService);
+  public readonly isAuthenticated$: Observable<boolean> = this.facade.isAuthenticated$
+  public readonly isDarkTheme$: Observable<boolean> = this.themeSwitch.isDarkTheme$;
+  public readonly username$: Observable<string> = this.facade.user$.pipe(map(user => user.name))
+  public readonly isAdmin$: Observable<boolean | null> = this.facade.isAdmin$
   public readonly userPhoto: Observable<string | undefined> = this.facade.user$.pipe(map(user => user.photo?.url))
 
   public photo: any
@@ -33,5 +45,9 @@ export class HeaderComponent implements OnInit{
 
   public onLogout() {
     this.facade.logout()
+  }
+
+  public onSwitchTheme() {
+    this.themeSwitch.switchTheme()
   }
 }
