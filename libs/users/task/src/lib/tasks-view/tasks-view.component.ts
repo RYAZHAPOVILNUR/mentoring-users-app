f48ad28c998c1ab89f9014ddef06a47c11ec411c
ONYXIY <onxvnezakona@gmail.com>
2023-08-18 01:21:28 +0300

    column from api via component state

--- libs/users/task/src/lib/tasks-view/tasks-view.component.ts
+++ libs/users/task/src/lib/tasks-view/tasks-view.component.ts
@@ -2,22 +2,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup} from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import { IColumn, ITask } from '@users/users/task/data-access';
+import { ITask } from '@users/users/task/data-access';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { Column } from '../tasks-view-container/tasks-list-container.store';
+
 
 
 @Component({
   selector: 'users-tasks-view',
   standalone: true,
-  imports: [CdkDrag, CdkDropList, NgFor, FormsModule, CdkDropListGroup, MatButtonModule, CommonModule, MatIconModule],
+  imports: [CdkDrag, CdkDropList, NgFor, FormsModule, CdkDropListGroup,MatCardModule, MatButtonModule, CommonModule, MatIconModule],
   templateUrl: './tasks-view.component.html',
   styleUrls: ['./tasks-view.component.scss'],
 })
 export class TasksViewComponent {
 
-  @Input({ required: true}) columns!: IColumn[] | null;
+  @Input({ required: true}) columns!: Column[] | null; 
+  @Input({required: true}) vm!: any;
   @Input() drop!: (event: CdkDragDrop<ITask[]>) => void;
+  @Input() tasks!: ITask[] 
   @Input() addTaskColumn!: () => void; //Возможо нужно убрать, в случае если можно реалзиовывать отрисовку внутри родиительской комп.
   @Input() deleteColumn!: (columnIndex: number) => void;
 }
