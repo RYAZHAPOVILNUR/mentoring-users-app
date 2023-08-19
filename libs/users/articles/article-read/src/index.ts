c62fbdd58fd852cabbf8915ad0fc46ef7fa2b4d8
Khamzat <xamzatseynaro@gmail.com>
2023-08-19 17:16:47 +0300

    bug

---
+++ libs/users/articles/article-read/src/index.ts
@@ -0,0 +1,4 @@
+/*
+ * Public API Surface of article-read
+ */
+export * from './lib/article-read/article-read.component';
---
+++ libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-article-read',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      article-read works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ArticleReadComponent {
+
+}
