8eddbf77fec52bfd4fa71f0cbdf18d1cb1845d2c
Seynaro <xamzatseynaro@gmail.com>
2023-08-21 14:39:16 +0300

    add article-read lib

--- libs/users/articles/article-read/src/lib/article-read-container/article-read-container.component.ts
+++ libs/users/articles/article-read/src/lib/article-read-container/article-read-container.component.ts
@@ -1,15 +1,45 @@
-import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Article, ArticlesActions, ArticleSelectors } from '../../../../data-access/src';
+import { selectQueryParam } from '../../../../../../core/data-access/src';
+import { map, Observable, withLatestFrom } from 'rxjs';
+import { LetDirective } from '@ngrx/component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ArticleReadComponent } from '../article-read/article-read.component';
+import { selectLoggedUserId } from '../../../../../../core/auth/data-access/src';
 
 @Component({
-  selector: 'lib-article-read-container',
+  selector: 'article-read-container',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ArticleReadComponent,
+    LetDirective,
+    MatProgressBarModule],
   templateUrl: './article-read-container.component.html',
   styleUrls: ['./article-read-container.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArticleReadContainerComponent {
   private readonly store = inject(Store);
+
+  public articleId$ = this.store.pipe(select(selectQueryParam('id')))
+  public readonly status$ = this.store.select(ArticleSelectors.selectStatus);
+  public readonly loggedUserId$ = this.store.select(selectLoggedUserId);
+
+  public viewedArticle$: Observable<Article | null> = this.store.select(ArticleSelectors.selectArticleForEdit)
+    .pipe(
+      withLatestFrom(this.articleId$),
+      map(([article, id]) => {
+        if (!article && id) {
+          this.store.dispatch(ArticlesActions.getArticle({ id }));
+        }
+        return article
+      })
+    );
+
+  constructor() {
+
+  }
 }
--- libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
+++ libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
@@ -1,14 +1,35 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuillModule } from "ngx-quill";
+import { MatCardModule } from "@angular/material/card";
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { Article } from '../../../../data-access/src';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
-  selector: 'lib-article-read',
+  selector: 'article-read',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    QuillModule,
+    MatCardModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './article-read.component.html',
   styleUrls: ['./article-read.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ArticleReadComponent {
 
+  @Input({ required: true }) article!: Article | null;
+  @Input({required: true}) loggedUserId!: number;
+
+  public clearArticleContent(content: string) {
+    return content.replace(/<[^>]*>/g, ' ')
+  };
+
+
 }
+
