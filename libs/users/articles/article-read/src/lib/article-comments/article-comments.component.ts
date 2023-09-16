eb786d7d2ba45492b87cd1dc8791b649456be09f
Ridook <ndgtpx@gmail.com>
2023-09-16 15:47:15 +0300

    task #83 feat(users): added the like logic
    Изменил архитектуру страницы статьи:
    вынес комментарии в отдельный компонент.
    И добавил смену локали даты в комментариях

--- libs/users/articles/article-read/src/lib/article-comments/article-comments.component.ts
+++ libs/users/articles/article-read/src/lib/article-comments/article-comments.component.ts
@@ -1,49 +1,69 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Comment } from '../../../../data-access/src';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { LoadingStatus } from '../../../../../../core/data-access/src';
+import {  ArticleCommentComponent } from './article-comment/article-comment.component';
+import '@angular/common/locales/global/ru';
 
 @Component({
   selector: 'article-comments',
   standalone: true,
   imports: [
-    CommonModule, 
-    MatFormFieldModule, 
-    ReactiveFormsModule, 
-    MatCardModule, 
-    MatInputModule, 
-    MatButtonModule, 
+    CommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ArticleCommentComponent
   ],
   templateUrl: './article-comments.component.html',
   styleUrls: ['./article-comments.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArticleCommentsComponent {
+export class ArticleCommentsComponent implements OnInit {
+
+  public likesCount: number = 0;
+  public isLikeActive: boolean = false;
   @Input() comments!: Comment[];
   @Input() status!: LoadingStatus;
   @Output() submitComment = new EventEmitter<string>();
 
   public formGroup = new FormGroup({
-    commentText: new FormControl('', [Validators.maxLength(100)])
-  })
+    commentText: new FormControl('', [Validators.maxLength(100)]),
+  });
 
   onSubmitComment() {
-    if(this.formGroup.valid && this.formGroup.value.commentText) {
-      this.submitComment.emit(
-        this.formGroup.value.commentText
-      )
-      this.formGroup.reset()
+    if (this.formGroup.valid && this.formGroup.value.commentText) {
+      this.submitComment.emit(this.formGroup.value.commentText);
+      this.formGroup.reset();
       return;
     }
     return;
-  }  
+  }
+  ngOnInit(): void {
+    console.log(this.comments, 'comments')
+  }
+
 }
