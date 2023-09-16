10a6799e1c007e92d63e99f4cfafca27bfaf229c
Ridook <ndgtpx@gmail.com>
2023-09-16 15:47:15 +0300

    task #83 feat(users): added the like logic
    Изменил архитектуру страницы статьи:
    вынес комментарии в отдельный компонент.
    И добавил смену локали даты в комментариях

---
+++ libs/users/articles/article-read/src/lib/article-comments/article-comment/article-comment.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { PushPipe } from '@ngrx/component';
+import { Comment } from '@users/users/articles/data-access';
+import { LanguageSwitchService } from '@users/users/core/ui/language-switch';
+@Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    PushPipe
+  ],
+  selector: 'article-comment',
+  templateUrl: './article-comment.component.html',
+  styleUrls: ['./article-comment.component.scss'],
+})
+export class ArticleCommentComponent {
+  private readonly languageService = inject(LanguageSwitchService);
+  public currentLanguage$ = this.languageService.selectedLanguage$.asObservable()
+  public likesCount: number = 0;
+  public isLikeActive: boolean = false;
+  @Input({ required: true }) comment!: Comment;
+
+  public onThumbUp() {
+    this.likesCount = this.likesCount === 0 ? 1 : 0;
+    this.isLikeActive = !this.isLikeActive;
+  }
+
+  public get avatarSrc(): string {
+    return this.comment.author.photo
+      ? this.comment.author.photo.url
+      : 'assets/img/1.png';
+  }
+
+  public get isLikesExists(): boolean {
+    return this.likesCount > 0;
+  }
+  public get currentLocale():string {
+    console.log(this.languageService.selectedLanguage$.value)
+    return this.languageService.selectedLanguage$.value
+  }
+}
