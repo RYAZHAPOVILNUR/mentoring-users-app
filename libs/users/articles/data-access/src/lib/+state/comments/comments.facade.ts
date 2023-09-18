ea8c29851398b2fb2f71c0eededb058d8b071767
rau4o <rauan.k@eventyr.pro>
2023-09-18 22:58:34 +0600

    task - 84 create 'see user profile'

--- libs/users/articles/data-access/src/lib/+state/comments/comments.facade.ts
+++ libs/users/articles/data-access/src/lib/+state/comments/comments.facade.ts
@@ -3,6 +3,7 @@ import { Store } from "@ngrx/store";
 import { selectComments, selectCommentsEntities } from "./comments.selectors";
 import { Observable } from "rxjs";
 import { Comment } from "../../models/user-comment.model";
+import {selectLoggedUserId} from "@auth/data-access";
 
 
 @Injectable({providedIn: 'root'})
@@ -10,4 +11,5 @@ export class CommentsFacade {
   private readonly store = inject(Store);
   public readonly articles$: Observable<Comment[]> = this.store.select(selectComments);
   public readonly articlesEntities$ = this.store.select(selectCommentsEntities);
+  public readonly loggedUser$ = this.store.select(selectLoggedUserId);
 }
