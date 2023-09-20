9c732ce954c2da7f4d3de5db7eac36aad60c72cc
Ridook <ndgtpx@gmail.com>
2023-09-20 16:56:34 +0300

    add user avatar in article

--- libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
+++ libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuillModule } from "ngx-quill";
 import { MatCardModule } from "@angular/material/card";
@@ -6,6 +6,9 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { Article } from '../../../../data-access/src';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthFacade } from '@auth/data-access';
+import { Observable, map } from 'rxjs';
+import { PushPipe } from '@ngrx/component';
 
 @Component({
   selector: 'article-read',
@@ -15,16 +18,19 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     RouterModule,
     MatButtonModule,
-    MatIconModule],
+    MatIconModule,
+    PushPipe
+  ],
   templateUrl: './article-read.component.html',
   styleUrls: ['./article-read.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ArticleReadComponent {
-
   @Input({ required: true }) article!: Article | null;
   @Input({required: true}) loggedUserId!: number;
 
+  private readonly authFacade = inject(AuthFacade);
+  public userPhoto$: Observable<string | undefined> = this.authFacade.user$.pipe(map(userData => userData.photo?.url))
   public clearArticleContent(content: string) {
     return content.replace(/<[^>]*>/g, ' ')
   };
