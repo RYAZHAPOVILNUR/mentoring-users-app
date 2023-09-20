ee0186a793ae1b4a616298c2e72d9089366ba6b4
rau4o <rauan.k@eventyr.pro>
2023-09-20 19:11:29 +0600

    task - 84 create 'see user profile page'

--- libs/users/profile/feature-profile/src/lib/user-profile-container/user-profile-container.component.ts
+++ libs/users/profile/feature-profile/src/lib/user-profile-container/user-profile-container.component.ts
@@ -3,9 +3,10 @@ import {CommonModule} from "@angular/common";
 import {ProfileFormUiComponent} from "../profile-form-ui/profile-form-ui.component";
 import {LetDirective} from "@ngrx/component";
 import {Store} from "@ngrx/store";
-import {ActivatedRoute} from "@angular/router";
-import {selectUserById} from "@users/users/data-access";
+import {UsersFacade} from "@users/users/data-access";
 import {selectAuthStatus} from "@auth/data-access";
+import {Observable, tap} from "rxjs";
+import {UsersEntity} from "@users/core/data-access";
 
 @Component({
   selector: 'users-profile-container',
@@ -22,10 +23,17 @@ import {selectAuthStatus} from "@auth/data-access";
 
 export class UserProfileContainerComponent {
 
+  private readonly usersFacade = inject(UsersFacade);
   private readonly store = inject(Store);
-  private readonly route = inject(ActivatedRoute);
 
   public readonly status$ = this.store.select(selectAuthStatus);
-  public readonly user$ = this.store.select((selectUserById(this.route.snapshot.params['id'])))
-
+  public readonly user$: Observable<UsersEntity | null> = this.usersFacade.openedUser$.pipe(
+    tap(
+      user => {
+        if (!user) {
+          this.usersFacade.loadUser()
+        }
+      }
+    )
+  );
 }
