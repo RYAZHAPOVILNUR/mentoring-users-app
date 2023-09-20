0d80356c4a91fc4ac6dac6407f9a7026e289eedd
youngeek19 <lenar.rashitov@internet.ru>
2023-09-20 16:36:04 +0300

    add avatar to article card

--- libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
+++ libs/users/articles/article-read/src/lib/article-read/article-read.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuillModule } from "ngx-quill";
 import { MatCardModule } from "@angular/material/card";
@@ -6,6 +6,9 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { Article } from '../../../../data-access/src';
 import { MatIconModule } from '@angular/material/icon';
+import { Observable, map } from 'rxjs';
+import { AuthFacade } from '@auth/data-access';
+import { PushPipe } from '@ngrx/component';
 
 @Component({
   selector: 'article-read',
@@ -15,7 +18,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     RouterModule,
     MatButtonModule,
-    MatIconModule],
+    MatIconModule,
+    PushPipe,
+  ],
   templateUrl: './article-read.component.html',
   styleUrls: ['./article-read.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
@@ -24,6 +29,10 @@ export class ArticleReadComponent {
 
   @Input({ required: true }) article!: Article | null;
   @Input({required: true}) loggedUserId!: number;
+
+  private readonly facade = inject(AuthFacade)
+  public readonly userPhoto: Observable<string | undefined> = this.facade.user$.pipe(map(user => user.photo?.url))
+  public readonly photo = this.userPhoto ? this.userPhoto : ''
 
   public clearArticleContent(content: string) {
     return content.replace(/<[^>]*>/g, ' ')
--- libs/users/articles/articles/src/lib/articles-view/articles-view.component.ts
+++ libs/users/articles/articles/src/lib/articles-view/articles-view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuillModule } from "ngx-quill";
 import { MatCardModule } from "@angular/material/card";
@@ -7,6 +7,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { Article } from '@users/users/articles/data-access';
 import { MatListModule } from "@angular/material/list";
+import { Observable, map } from 'rxjs';
+import { AuthFacade } from '@auth/data-access';
+import { PushPipe } from '@ngrx/component';
 
 @Component({
   selector: 'users-articles-view',
@@ -16,7 +19,8 @@ import { MatListModule } from "@angular/material/list";
     MatCardModule,
     RouterModule,
     MatButtonModule,
-    MatIconModule, MatListModule
+    MatIconModule, MatListModule,
+    PushPipe
   ],
   templateUrl: './articles-view.component.html',
   styleUrls: ['./articles-view.component.scss'],
@@ -25,6 +29,10 @@ import { MatListModule } from "@angular/material/list";
 export class ArticlesViewComponent {
   @Input({ required: true }) articles!: Article[];
   @Input({required: true}) loggedUserId!: number;
+
+  private readonly facade = inject(AuthFacade)
+  public readonly userPhoto: Observable<string | undefined> = this.facade.user$.pipe(map(user => user.photo?.url))
+  public readonly photo = this.userPhoto ? this.userPhoto : ''
 
   public clearArticleContent(content: string) {
     return content.replace(/<[^>]*>/g, ' ')
