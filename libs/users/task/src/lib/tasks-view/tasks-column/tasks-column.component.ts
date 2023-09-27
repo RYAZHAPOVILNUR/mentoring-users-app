b5a1b319ee84bf2ec4162d59958d9adb1250fb27
Ridook <ndgtpx@gmail.com>
2023-09-27 19:41:19 +0300

    добавил возможность редактирования имени колонки

--- libs/users/task/src/lib/tasks-view/tasks-column/tasks-column.component.ts
+++ libs/users/task/src/lib/tasks-view/tasks-column/tasks-column.component.ts
@@ -1,11 +1,106 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDragPreview,
+  CdkDropList,
+  CdkDropListGroup,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { IColumn } from '@users/users/task/data-access';
 
 @Component({
   selector: 'users-tasks-column',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CdkDrag,
+    CdkDropList,
+    FormsModule,
+    CdkDropListGroup,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CdkDragPreview,
+    DragDropModule,
+  ],
   templateUrl: './tasks-column.component.html',
   styleUrls: ['./tasks-column.component.scss'],
 })
-export class TasksColumnComponent {}
+export class TasksColumnComponent {
+  @Input({ required: true })
+  set column(column: IColumn) {
+    this.title = column.columnName;
+    this.columnData = column;
+  }
+  get column() {
+    return this.columnData;
+  }
+  @Output() deleteColumn = new EventEmitter<number>();
+  @Output() deleteTask = new EventEmitter<{
+    columnIndex: number;
+    taskName: string;
+  }>();
+  @Output() dragDrop = new EventEmitter<CdkDragDrop<IColumn>>();
+  @Output() addTask = new EventEmitter();
+  @Output() removeTask = new EventEmitter();
+  @ViewChild('input', { read: ElementRef })
+  set input(elRef: ElementRef<HTMLInputElement>) {
+    if (elRef) {
+      elRef.nativeElement.focus();
+    }
+  }
+  public editing = false;
+  public title!: string;
+  private columnData!: IColumn;
+
+  onDragDrop($event: CdkDragDrop<IColumn>) {
+    this.dragDrop.emit($event);
+  }
+
+  public onAddNewTaskModal(): void {
+    this.addTask.emit();
+  }
+
+  public onRemoveTask(taskData: any) {
+    this.removeTask.emit(taskData);
+  }
+
+  public splitText(text: string): string {
+    let result = '';
+    let line = '';
+
+    text.split(' ').forEach((word) => {
+      if (line.length + word.length <= 30) {
+        line += (line ? ' ' : '') + word;
+      } else {
+        result += (result ? '<br>' : '') + line;
+        line = word;
+      }
+    });
+    return result + (line ? '<br>' + line : '');
+  }
+
+  public removeColumn() {
+    this.deleteColumn.emit();
+  }
+  public onEditing() {
+    this.editing = !this.editing;
+  }
+}
