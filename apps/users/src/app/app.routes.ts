3a3baae3b82ab52b5e2dffede97001dd045986fc
Pikhtyarev <pihtyarevv@mail.ru>
2023-09-30 14:17:40 +0300

    добавил кнопки и выбор цвета в настройках

--- apps/users/src/app/app.routes.ts
+++ apps/users/src/app/app.routes.ts
@@ -7,8 +7,8 @@ import { canDeactivateFormComponent } from '@users/core/utils';
 const layoutAgnosticComponents = [
   {
     path: 'home',
-    loadComponent: () => import('@users/home').then(c => c.HomeComponent)
-  }
+    loadComponent: () => import('@users/home').then((c) => c.HomeComponent),
+  },
 ];
 
 export const appRoutes: Route[] = [
@@ -23,59 +23,91 @@ export const appRoutes: Route[] = [
         children: [
           {
             path: 'users',
-            loadComponent: () => import('@users/feature-users-list').then(c => c.UsersListContainerComponent)
+            loadComponent: () =>
+              import('@users/feature-users-list').then(
+                (c) => c.UsersListContainerComponent
+              ),
           },
           {
             path: 'users/:id',
-            loadComponent: () => import('@users/feature-users-detail').then(c => c.UsersDetailComponent),
-          }
+            loadComponent: () =>
+              import('@users/feature-users-detail').then(
+                (c) => c.UsersDetailComponent
+              ),
+          },
         ],
       },
       {
         path: 'profile',
-        loadComponent: () => import('@users/users/profile/feature-profile').then(c => c.ProfileContainerComponent),
+        loadComponent: () =>
+          import('@users/users/profile/feature-profile').then(
+            (c) => c.ProfileContainerComponent
+          ),
       },
       {
         path: 'profile/:id',
-        loadComponent: () => import('@users/users/profile/feature-profile').then(c => c.UserProfileContainerComponent),
+        loadComponent: () =>
+          import('@users/users/profile/feature-profile').then(
+            (c) => c.UserProfileContainerComponent
+          ),
       },
       {
         path: 'article-editor',
-        loadComponent: () => import('@users/users/articles/articles-create').then(c => c.ArticlesCreateContainerComponent),
-        canDeactivate: [canDeactivateFormComponent]
+        loadComponent: () =>
+          import('@users/users/articles/articles-create').then(
+            (c) => c.ArticlesCreateContainerComponent
+          ),
+        canDeactivate: [canDeactivateFormComponent],
       },
       {
         path: 'articles',
-        loadComponent: () => import('@users/users/articles/articles').then(c => c.ArticlesViewContainerComponent),
+        loadComponent: () =>
+          import('@users/users/articles/articles').then(
+            (c) => c.ArticlesViewContainerComponent
+          ),
       },
       {
         path: 'articles/:id',
-        loadComponent: () => import('@users/users/articles/article-read').then(c => c.ArticleReadContainerComponent),
+        loadComponent: () =>
+          import('@users/users/articles/article-read').then(
+            (c) => c.ArticleReadContainerComponent
+          ),
       },
       {
         path: 'tasks',
-        loadComponent: () => import('@users/users/task').then(c => c.TasksContainerComponent)
+        loadComponent: () =>
+          import('@users/users/task').then((c) => c.TasksContainerComponent),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('@users/users/settings/feature-change-color').then(
+            (c) => c.ChangeColorComponent
+          ),
       },
       {
         path: 'chart',
-        loadComponent: () => import('@users/users/chart').then(c => c.ChartViewComponent)
+        loadComponent: () =>
+          import('@users/users/chart').then((c) => c.ChartViewComponent),
       },
-      ...layoutAgnosticComponents
-    ]
+      ...layoutAgnosticComponents,
+    ],
   },
   {
     path: 'guest',
     component: UnauthorizedUserLayoutComponent,
-    children: [
-      ...layoutAgnosticComponents
-    ]
+    children: [...layoutAgnosticComponents],
   },
   {
     path: 'login',
-    loadComponent: () => import('@auth/feature-login').then(c => c.LoginContainerComponent)
+    loadComponent: () =>
+      import('@auth/feature-login').then((c) => c.LoginContainerComponent),
   },
   {
     path: 'signup',
-    loadComponent: () => import('@auth/feature-register').then(c => c.RegisterContainerComponent)
+    loadComponent: () =>
+      import('@auth/feature-register').then(
+        (c) => c.RegisterContainerComponent
+      ),
   },
 ];
