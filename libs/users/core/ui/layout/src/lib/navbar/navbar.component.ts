dc036b3add1f3f8655f6ec4fd902de1fea24a3f4
Ridook <ndgtpx@gmail.com>
2023-10-02 15:41:40 +0300

    сделал открытие аватарки по клику крупным планом

--- libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
+++ libs/users/core/ui/layout/src/lib/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -17,23 +17,30 @@ import { PushPipe } from '@ngrx/component';
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatDialogModule,
     MatListModule,
     RouterModule, 
     TranslateModule,
-    PushPipe
-    
+    PushPipe,
+    MatDialogModule,
+    MatIconModule
+  
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  @ViewChild('photo') avatar!: any
+  public dialog = inject(MatDialog)
   private readonly facade = inject(AuthFacade)
   public readonly userPhoto: Observable<string | undefined> = this.facade.user$.pipe(map(user => user.photo?.url))
   public readonly photo = this.userPhoto ? this.userPhoto : ''
 
   public onLogout() {
     this.facade.logout()
+  }
+
+  public openAvatar() {
+    this.dialog.open(this.avatar)
   }
 }
