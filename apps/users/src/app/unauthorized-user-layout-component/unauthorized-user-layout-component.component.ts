6be971549b25eb0900fbe0d96e0239105e80d7fc
Ridook <ndgtpx@gmail.com>
2023-10-02 11:26:04 +0300

    edit buttons guest/home

--- apps/users/src/app/unauthorized-user-layout-component/unauthorized-user-layout-component.component.ts
+++ apps/users/src/app/unauthorized-user-layout-component/unauthorized-user-layout-component.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'users-unauthorized-user-layout-component',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ],
   templateUrl: './unauthorized-user-layout-component.component.html',
   styleUrls: ['./unauthorized-user-layout-component.component.scss'],
