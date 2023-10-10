e63d39f084392774b2e807a177d15b08265e090a
Sultanefendi <sedredinovich@gmail.com>
2023-10-10 21:56:36 +0300

    added the ability to switch theme

--- libs/users/settings/src/lib/settings/settings.component.ts
+++ libs/users/settings/src/lib/settings/settings.component.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { LanguageKeys, LanguageSwitchService } from '@users/users/core/ui/language-switch';
 import { PushPipe } from '@ngrx/component';
+import { ThemeSwitchService } from '@users/users/core/ui/theme-switch';
+import { Observable } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'users-settings',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, PushPipe],
+  imports: [CommonModule, MatButtonModule, PushPipe, MatIconModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,10 +19,13 @@ import { PushPipe } from '@ngrx/component';
 export class SettingsComponent {
   private readonly languageSwitchService = inject(LanguageSwitchService);
   public readonly selectedLanguage$ = this.languageSwitchService.selectedLanguage$
-  
+  private readonly themeSwitchService = inject(ThemeSwitchService);
+  public readonly isDarkTheme$: Observable<boolean> = this.themeSwitchService.isDarkTheme$;
   public articleStyle = 'LIST'
 
-
+  public onSwitchTheme() {
+    this.themeSwitchService.switchTheme()
+  }
 
   public onSwitchLanguage(language: LanguageKeys) {
     this.languageSwitchService.setLanguage(language);
