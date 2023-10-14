e11fd12340f70c4a6826c362635cf0cf5be2dd94
rashidovD <61225454+rashidovD@users.noreply.github.com>
2023-10-14 20:35:18 +0300

    убрал лишние логи и комментарии

--- libs/users/backlog/data-access/src/lib/+state/backlog.action.ts
+++ libs/users/backlog/data-access/src/lib/+state/backlog.action.ts
@@ -1,7 +1,5 @@
 import { createActionGroup, props, emptyProps, createAction } from "@ngrx/store";
-import { Article } from "@users/users/articles/data-access";
 import { CreateBacklog, IBacklog } from "../model/backlog.model";
-import { CreateUserDTO, UsersEntity } from "@users/core/data-access";
 
 export const backlogAction = createActionGroup({
   source: 'backlog',
@@ -12,28 +10,5 @@ export const backlogAction = createActionGroup({
     deleteBacklogSuccess: props<{ id: number }>(),
     addBacklog: props<{ backlogData: CreateBacklog }>(),
     addBacklogSuccess: props<{ backlogData: IBacklog }>()
-
-    //
-    // export const addUser = createAction('[Users Page] Add User', props<{ userData: CreateUserDTO }>());
-    // export const addUserSuccess = createAction('[Users/Api] Add User Success', props<{ userData: UsersEntity }>());
-    // export const addUserFailed = createAction('[Users/Api] Add User Failed', props<{ error: any }>());
-
-    // loadBacklogSuccess: ,
-    // loadBacklogSuccess: props<{ boards: ITaskBoard[] }>(),
-    // loadBacklogSuccess: props<{ boards: ITaskBoard[] }>(),
-
-    // loadMyBoard: emptyProps(),
-    // loadMyBoardSuccess: props<{ board: ITaskBoard }>(),
-    //
-    // addBoard: props<ITaskBoard>(),
-    // addBoardSuccess: props<ITaskBoard>(),
-    //
-    // updateColumns: props<{columns: IColumn[]}>(),
-    // updateColumnsSuccess: props<{columns: IColumn[]}>(),
-    //
-    // deleteColumn: props<{ columnIndex: number }>(),
-    // deleteColumnSuccess: props<{ columnIndex: number }>(),
-    //
-    // searchTask: props<{term: string}>(),
   }
 });
