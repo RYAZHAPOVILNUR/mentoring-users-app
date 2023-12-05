c0bd4896d38de1b05927f88af5b5d741fb1980f0
OneScriptMan <vladzam79@gmail.com>
2023-12-05 12:18:49 +0300

    feat: add start files for user-filter

---
+++ libs/users/users/feature-users-list/src/lib/user-filter/user-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'users-user-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-filter.component.html',
+  styleUrls: ['./user-filter.component.scss'],
+})
+export class UserFilterComponent {}
