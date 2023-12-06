e0e18f37fbefbbbfbda04cace7e11e0ad1a2a300
OneScriptMan <vladzam79@gmail.com>
2023-12-06 14:38:52 +0300

    feat: add input and button

--- libs/users/users/feature-users-list/src/lib/user-filter/user-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/user-filter/user-filter.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'users-user-filter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   templateUrl: './user-filter.component.html',
   styleUrls: ['./user-filter.component.scss'],
 })
-export class UserFilterComponent {}
+export class UserFilterComponent {
+  nameFilterControl = new FormControl('');
+  value = '';
+
+  applyFilter() {
+    const filterValue = this.nameFilterControl.value;
+  }
+}
