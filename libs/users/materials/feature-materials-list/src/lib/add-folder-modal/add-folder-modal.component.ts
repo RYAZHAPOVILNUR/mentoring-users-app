5016331c4b8d600704c22a272fa54518b0f495fa
dmitryPatseyeu <dmitrypatseyeu@gmail.com>
2024-02-23 17:50:21 +0100

    fix: restyle add folder modal

--- libs/users/materials/feature-materials-list/src/lib/add-folder-modal/add-folder-modal.component.ts
+++ libs/users/materials/feature-materials-list/src/lib/add-folder-modal/add-folder-modal.component.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ADD_FOLDER_LIMITS } from '../../../../util/constant';
 import { FormErrorStateMatcher } from '../../../../util/exceptions';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'add-folder-modal',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatDialogModule, MatInputModule, ReactiveFormsModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule, MatInputModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './add-folder-modal.component.html',
   styleUrls: ['./add-folder-modal.component.scss']
 })
