7ae987609a44db08fb203cc9d726704f344933bd
airatmm <airatmm@gmail.com>
2024-03-11 22:32:50 +0400

    feat: folders rendering

--- libs/users/materials/feature-folders-list/src/lib/folder/folder.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folder/folder.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Folder } from '@users/materials-data-access';
 
 @Component({
   selector: 'users-folder',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './folder.component.html',
   styleUrls: ['./folder.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FolderComponent {}
+export class FolderComponent {
+  @Input({ required: true }) public folder!: Folder;
+}
--- libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Folder } from '@users/materials-data-access';
+import { FolderComponent } from '../folder/folder.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'users-folders-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FolderComponent, MatIconModule, MatListModule, MatCardModule],
   templateUrl: './folders-list.component.html',
   styleUrls: ['./folders-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
