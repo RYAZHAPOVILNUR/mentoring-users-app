4b75aadc87ccfe512b3674ce60ebc0eaf860a0a5
airatmm <airatmm@gmail.com>
2024-03-11 11:10:57 +0400

    wip: selectMaterialsState and selectAllFolders - selectors. delete spec

--- libs/users/materials/data-access/src/lib/+state/materials.selectors.ts
+++ libs/users/materials/data-access/src/lib/+state/materials.selectors.ts
@@ -1,4 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromMaterials from './materials.reducer';
+import { MaterialsFeatureState } from './materials.reducer';
 
-export const selectMaterialsState = createFeatureSelector<fromMaterials.State>(fromMaterials.materialsFeatureKey);
+export const selectMaterialsState = createFeatureSelector<fromMaterials.MaterialsFeatureState>(
+  fromMaterials.MATERIALS_FEATURE_KEY
+);
+
+export const selectAllFolders = createSelector(selectMaterialsState, (state: MaterialsFeatureState) => state.folders);
