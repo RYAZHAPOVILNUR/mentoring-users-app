b5d66f4a9498b91b92fea03c26cf435741d6fbb1
airatmm <airatmm@gmail.com>
2024-03-12 12:10:54 +0400

    feat: add-folder-dialog

--- libs/users/materials/feature-create-folder/src/lib/add-folder-dialog/add-folder-dialog.component.ts
+++ libs/users/materials/feature-create-folder/src/lib/add-folder-dialog/add-folder-dialog.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'users-add-folder-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './add-folder-dialog.component.html',
   styleUrls: ['./add-folder-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddFolderDialogComponent {}
+export class AddFolderDialogComponent {
+  public dialog = inject(MatDialog);
+  public folderName: FormControl = new FormControl('', Validators.required);
+}
