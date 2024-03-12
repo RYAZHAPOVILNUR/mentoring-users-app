992592cc9b46c45a9091b9da9971fa83e12dbf6e
airatmm <airatmm@gmail.com>
2024-03-12 11:42:09 +0400

    wip: AddFolderButtonComponent style

--- libs/users/materials/feature-create-folder/src/lib/add-folder-button/add-folder-button.component.ts
+++ libs/users/materials/feature-create-folder/src/lib/add-folder-button/add-folder-button.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'users-add-folder-button',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './add-folder-button.component.html',
   styleUrls: ['./add-folder-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddFolderButtonComponent {}
+export class AddFolderButtonComponent {
+  public openCreateFolderDialog(): void {
+    console.log('click openCreateFolderDialog');
+  }
+}
