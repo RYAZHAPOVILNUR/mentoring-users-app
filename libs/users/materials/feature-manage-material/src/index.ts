12b6f5298cf77669b3d2a88d33879b369e80cfa4
airatmm <airatmm@gmail.com>
2024-03-20 11:05:55 +0400

    ref: open-material-dialog

--- libs/users/materials/feature-manage-material/src/index.ts
+++ libs/users/materials/feature-manage-material/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/feature-manage-material';
 export * from './lib/add-material-button/add-material-button.component';
 export * from './lib/add-material-dialog/add-material-dialog.component';
 export * from './lib/delete-material-dialog/delete-material-dialog.component';
+export * from './lib/open-material-dialog/open-material-dialog.component';
--- libs/users/materials/feature-manage-material/src/lib/open-material-dialog/open-material-dialog.component.ts
+++ libs/users/materials/feature-manage-material/src/lib/open-material-dialog/open-material-dialog.component.ts
@@ -2,11 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'users-open-materal-dialog',
+  selector: 'users-open-material-dialog',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './open-materal-dialog.component.html',
-  styleUrls: ['./open-materal-dialog.component.scss'],
+  templateUrl: './open-material-dialog.component.html',
+  styleUrls: ['./open-material-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OpenMateralDialogComponent {}
+export class OpenMaterialDialogComponent {}
