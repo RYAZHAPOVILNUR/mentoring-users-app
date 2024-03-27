a64f70245610efe1cdce89ab67db186265ea8e0d
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-27 23:54:53 +0600

    feat: create/implement users-filter component

---
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'users-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users-filter.component.html',
+  styleUrls: ['./users-filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersFilterComponent {}
