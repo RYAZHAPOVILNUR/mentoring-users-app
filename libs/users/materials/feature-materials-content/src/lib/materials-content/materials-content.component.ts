c464704c1c63a963cf4eb33495f79abcefe86ead
SoNomad <110348044+SoNomad@users.noreply.github.com>
2024-05-27 03:41:33 +0300

    немного стилей

--- libs/users/materials/feature-materials-content/src/lib/materials-content/materials-content.component.ts
+++ libs/users/materials/feature-materials-content/src/lib/materials-content/materials-content.component.ts
@@ -14,24 +14,41 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialsDTO } from '@users/core/data-access';
 import { DomSanitizer } from '@angular/platform-browser';
+import {
+  NgxExtendedPdfViewerModule,
+  NgxExtendedPdfViewerService,
+} from 'ngx-extended-pdf-viewer';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'users-materials-content',
   standalone: true,
-  imports: [CommonModule, PdfViewerModule, MatDialogModule, MatIconModule],
+  imports: [
+    CommonModule,
+    PdfViewerModule,
+    MatDialogModule,
+    MatIconModule,
+    NgxExtendedPdfViewerModule,
+    MatButtonModule,
+  ],
   templateUrl: './materials-content.component.html',
   styleUrls: ['./materials-content.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
-  // changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MaterialsContentComponent {
   private readonly dialogRef = inject(MatDialogRef<MaterialsContentComponent>);
   public readonly data: MaterialsDTO = inject(MAT_DIALOG_DATA);
   private readonly sanitazer = inject(DomSanitizer);
+  public readonly pdfViewer = inject(NgxExtendedPdfViewerService);
 
   public link = this.sanitazer.bypassSecurityTrustResourceUrl(
     this.data.material_link
   );
+
+  constructor() {
+    console.log(this.link);
+  }
 
   public close(): void {
     this.dialogRef.close();
