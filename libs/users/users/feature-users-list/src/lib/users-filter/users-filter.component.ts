de2a654e94582002bced5fcdbfc713604906de52
baglana <102351104+baglana@users.noreply.github.com>
2024-06-04 19:27:14 +0500

    [#3] feat: create users-filter component

---
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users-filter',
+  templateUrl: './users-filter.component.html',
+  styleUrls: ['./users-filter.component.scss'],
+  standalone: true,
+})
+export class UsersFilterComponent {}
--- libs/users/users/feature-users-list/src/lib/users-list-container/users-list-container.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-list-container/users-list-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersListComponent } from '../users-list/users-list.component';
 import { UsersListContainerStore } from './users-list-container.store';
@@ -9,6 +9,7 @@ import { UsersFacade } from '@users/users/data-access';
 import { Router } from '@angular/router';
 import { LetDirective } from '@ngrx/component';
 import { CreateUsersButtonComponent } from '@users/feature-users-create';
+import { UsersFilterComponent } from '../users-filter/users-filter.component';
 
 @Component({
   selector: 'users-list-container',
@@ -20,6 +21,7 @@ import { CreateUsersButtonComponent } from '@users/feature-users-create';
     MatDialogModule,
     LetDirective,
     CreateUsersButtonComponent,
+    UsersFilterComponent,
   ],
   templateUrl: './users-list-container.component.html',
   styleUrls: ['./users-list-container.component.scss'],
@@ -28,12 +30,12 @@ import { CreateUsersButtonComponent } from '@users/feature-users-create';
   providers: [UsersListContainerStore],
 })
 export class UsersListContainerComponent {
-  private readonly componentStore = inject(UsersListContainerStore);
   public usersFacade = inject(UsersFacade);
+  public readonly loggedUser$ = this.usersFacade.loggedUser$;
+  private readonly componentStore = inject(UsersListContainerStore);
   public readonly users$ = this.componentStore.users$;
   public readonly status$ = this.componentStore.status$;
   public readonly errors$ = this.componentStore.errors$;
-  public readonly loggedUser$ = this.usersFacade.loggedUser$;
   private readonly router = inject(Router);
 
   onDeleteUser(user: UsersVM) {
