8325234f47f0623761dfdc311baa8006833d7066
baglana <102351104+baglana@users.noreply.github.com>
2024-06-05 06:35:40 +0500

    [#3] feat: add formControl for name

--- libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'users-filter',
   templateUrl: './users-filter.component.html',
   styleUrls: ['./users-filter.component.scss'],
   standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
 })
-export class UsersFilterComponent {}
+export class UsersFilterComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  filterForm = this.fb.group({ name: '' });
+
+  ngOnInit() {
+    this.filterForm.get('name')?.valueChanges.subscribe((value) => console.log(value));
+  }
+}
