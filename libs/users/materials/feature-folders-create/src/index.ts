2ffd20b34198862acba022db043c5fa028356287
baglana <102351104+baglana@users.noreply.github.com>
2024-06-16 15:24:24 +0500

    [#2] feat(folders): generate feature-folders-create components

--- libs/users/materials/feature-folders-create/src/index.ts
+++ libs/users/materials/feature-folders-create/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/feature-folders-create.module';
+export * from './lib/folders-add-button/folders-add-button.component';
+export * from './lib/folders-add-dialog/folders-add-dialog.component';
