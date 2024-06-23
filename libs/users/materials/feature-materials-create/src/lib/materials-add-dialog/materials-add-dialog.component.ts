129721cf595061d045823909222d4837e3068002
baglana <102351104+baglana@users.noreply.github.com>
2024-06-23 16:09:05 +0500

    [#2] feat(materials): add materials-add-dialog

--- libs/users/materials/feature-materials-create/src/lib/materials-add-dialog/materials-add-dialog.component.ts
+++ libs/users/materials/feature-materials-create/src/lib/materials-add-dialog/materials-add-dialog.component.ts
@@ -1,12 +1,33 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'users-materials-add-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatDialogModule, MatInputModule, MatButtonModule],
   templateUrl: './materials-add-dialog.component.html',
   styleUrls: ['./materials-add-dialog.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class MaterialsAddDialogComponent {}
+export class MaterialsAddDialogComponent {
+  public type!: string;
+  private readonly dialogRef = inject(MatDialogRef<MaterialsAddDialogComponent>);
+
+  private readonly fb = inject(FormBuilder);
+  public readonly formGroup = this.fb.group({
+    title: ['', Validators.required],
+    url: ['', Validators.required],
+  });
+
+  public onAddMaterial() {
+    if (this.formGroup.valid) {
+      this.dialogRef.close(this.formGroup.value);
+    }
+  }
+
+  private validateUrl();
+}
