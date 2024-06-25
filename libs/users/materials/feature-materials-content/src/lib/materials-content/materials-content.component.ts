f8c8adf359c98fe58f6e2211c49174ff91711c3d
baglana <102351104+baglana@users.noreply.github.com>
2024-06-25 12:32:40 +0500

    [#2] fix(materials): try to fix sanitize error

--- libs/users/materials/feature-materials-content/src/lib/materials-content/materials-content.component.ts
+++ libs/users/materials/feature-materials-content/src/lib/materials-content/materials-content.component.ts
@@ -6,11 +6,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { Material, regex } from '@users/materials/data-access';
+import { SafePipe } from 'safe-pipe';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'users-materials-content',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule, MatCardModule, MatIconModule, PdfViewerModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatCardModule, MatIconModule, PdfViewerModule, SafePipe],
   templateUrl: './materials-content.component.html',
   styleUrls: ['./materials-content.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
@@ -19,4 +21,9 @@ import { Material, regex } from '@users/materials/data-access';
 export class MaterialsContentComponent {
   public readonly data: Material = inject(MAT_DIALOG_DATA);
   public readonly regex = regex;
+  protected safeVideoUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.data.material_link as string);
+  }
 }
