12f9cfaf5fe9b65e626128b0b6f7880e39d5f896
Oleg Fedorov <fedoroleg@gmail.com>
2024-07-28 17:13:01 +0300

    feat(users-filter) add users-filter component and input element

---
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'users-filter',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule],
+  templateUrl: './users-filter.component.html',
+  styleUrls: ['./users-filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersFilterComponent {}
