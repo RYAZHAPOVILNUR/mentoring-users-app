f97ae2b2f09be259c839c865d5546e248836ca10
Oleg Fedorov <fedoroleg@gmail.com>
2024-08-07 17:10:55 +0300

    chore(folders-list) add mock bootstrap cards for tests

--- libs/users/materials/feature-folders-list/src/lib/folders-card/folders-card.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-card/folders-card.component.ts
@@ -1,12 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'folders-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule, MatMenuModule],
   templateUrl: './folders-card.component.html',
   styleUrls: ['./folders-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FoldersCardComponent {}
+export class FoldersCardComponent {
+  text = 'Test Text';
+}
--- libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoldersCardComponent } from "../folders-card/folders-card.component";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'folders-list',
   standalone: true,
-  imports: [CommonModule, FoldersCardComponent],
+  imports: [CommonModule, FoldersCardComponent, CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule, MatMenuModule],
   templateUrl: './folders-list.component.html',
   styleUrls: ['./folders-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
