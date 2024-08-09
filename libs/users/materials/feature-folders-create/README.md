2fbfcc6ac14257a073bc9f500c2f1c8279eb1765
Oleg Fedorov <fedoroleg@gmail.com>
2024-08-09 15:30:09 +0300

    feat(feature-folders-create) add feature-folders-create library

---
+++ libs/users/materials/feature-folders-create/README.md
@@ -0,0 +1,7 @@
+# folders-add-button
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test folders-add-button` to execute the unit tests.
---
+++ libs/users/materials/feature-folders-create/src/lib/folders-add-button/folders-add-button.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './folders-add-button.component.html',
+  styleUrls: ['./folders-add-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FoldersAddButtonComponent {}
