ddf5da358304aaba8f138ddbf147a9ac1ccda8d4
77001 <alimzhanzhakashev@gmail.com>
2024-08-17 21:46:48 +0500

    создал исходное состояние

--- libs/users/materials/data-access/src/lib/+state/materials.selectors.ts
+++ libs/users/materials/data-access/src/lib/+state/materials.selectors.ts
@@ -1,4 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromMaterials from './materials.reducer';
+import { MATERIALS_FEATURE_KEY, MaterialsState, materialsAdapter } from './materials.reducer';
+export const selectMaterialsState = createFeatureSelector<MaterialsState>(MATERIALS_FEATURE_KEY);
 
-export const selectMaterialsState = createFeatureSelector<fromMaterials.State>(fromMaterials.materialsFeatureKey);
+const { selectAll, selectEntities } = materialsAdapter.getSelectors();
+
+export const selectMaterialsStatus = createSelector(selectMaterialsState, (state: MaterialsState) => state.status);
+
+export const selectAllFolders = createSelector(selectMaterialsState, (state: MaterialsState) => selectAll(state));
