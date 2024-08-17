28affee195232107e8d2cfc35e49f5805eae028f
77001 <alimzhanzhakashev@gmail.com>
2024-08-17 21:34:39 +0500

    создал архитектуру

---
+++ libs/users/materials/feature-materials-folders/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/feature-materials-folders.module';
+export * from './lib/folders-add-button/folders-add-button.component';
+export * from './lib/folders-card/folders-card.component';
+export * from './lib/folders-list/folders-list.component';
+export * from './lib/folders-list-container/folders-list-container.component';
---
+++ libs/users/materials/feature-materials/README.md
@@ -0,0 +1,7 @@
+# feature-materials
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-materials` to execute the unit tests.
