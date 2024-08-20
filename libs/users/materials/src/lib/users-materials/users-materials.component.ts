d3d64d14b4362135ce231d88b0d2518eb7a2340c
Timur Ayupov <t.r.ayupov@gmail.com>
2024-08-20 21:27:29 +0500

    test commit

--- libs/users/materials/src/lib/users-materials/users-materials.component.ts
+++ libs/users/materials/src/lib/users-materials/users-materials.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CreateMaterialsButtonComponent } from '@users/materials/feature-materials-create';
+import { MaterialsFacade } from '@users/materials/data-access';
 
 @Component({
   selector: 'users-users-materials',
@@ -12,4 +13,9 @@ import { CreateMaterialsButtonComponent } from '@users/materials/feature-materia
   styleUrls: ['./users-materials.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersMaterialsComponent {}
+export class UsersMaterialsComponent {
+  private readonly materialsFacade = inject(MaterialsFacade);
+  public readonly status$ = this.materialsFacade.status$;
+  public readonly error$ = this.materialsFacade.error$;
+  public readonly folder$ = this.materialsFacade.folder$;
+}
