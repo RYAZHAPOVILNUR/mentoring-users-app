3f176061c1ba4384c383110a401df00c770c2d24
Musa Rufatuly <rufatulymusa567@gmail.com>
2024-08-24 22:30:17 +0500

    test

--- libs/users/materials/feature-folders-list/src/lib/folders-card/folders-card.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-card/folders-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { FoldersVM } from 'libs/users/materials/folders-vm';
 
