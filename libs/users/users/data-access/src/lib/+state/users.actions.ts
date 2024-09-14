3b4fcc6def9dd809ebac642ee47049741ae63160
Albert <albertkhamzin93@gmail.com>
2024-09-14 11:24:15 +0300

    refactor(users): fix formatting

--- libs/users/users/data-access/src/lib/+state/users.actions.ts
+++ libs/users/users/data-access/src/lib/+state/users.actions.ts
@@ -4,32 +4,47 @@ import { CreateUserDTO, LoadingStatus, UsersDTO, UsersEntity } from '@users/core
 
 export type onSuccessEditionCbType = () => void;
 
-export const initUsers = createAction(
-  '[Users Page] Init');
+export const initUsers = createAction('[Users Page] Init');
 
 export const loadUsersSuccess = createAction(
-  '[Users/API] Load Users Success', props<{ users: UsersEntity[] }>());
+  '[Users/API] Load Users Success',
+  props<{ users: UsersEntity[] }>()
+);
 
 export const loadUsersFailure = createAction(
-  '[Users/API] Load Users Failure', props<{ error: any }>());
+  '[Users/API] Load Users Failure',
+  props<{ error: any }>()
+);
 
 export const deleteUser = createAction(
-  '[Users Page] Delete User', props<{ id: number }>());
+  '[Users Page] Delete User',
+  props<{ id: number }>()
+);
+
 export const deleteUserSuccess = createAction(
-  '[Users/Api] Delete User Success', props<{ id: number }>());
+  '[Users/Api] Delete User Success',
+  props<{ id: number }>()
+);
+
 export const deleteUserFailed = createAction(
-  '[Users/Api] Delete User Failed', props<{ error: any }>());
+  '[Users/Api] Delete User Failed',
+  props<{ error: any }>()
+);
 
 export const addUser = createAction(
-  '[Users Page] Add User', props<{ userData: CreateUserDTO }>());
+  '[Users Page] Add User',
+  props<{ userData: CreateUserDTO }>()
+);
+
 export const addUserSuccess = createAction(
-  '[Users/Api] Add User Success', props<{ userData: UsersEntity }>());
+  '[Users/Api] Add User Success',
+  props<{ userData: UsersEntity }>()
+);
+
 export const addUserFailed = createAction(
-  '[Users/Api] Add User Failed', props<{ error: any }>());
-
-// export const selectId = createAction('[Users Page] Select Id', props<{ id: number }>());
-
-// export const deleteSelectedId = createAction('[Users Page] Delete Selected Id');
+  '[Users/Api] Add User Failed',
+  props<{ error: any }>()
+);
 
 export const editUser = createAction(
   '[Users Detail] Edit User',
@@ -39,20 +54,34 @@ export const editUser = createAction(
     onSuccessCb: onSuccessEditionCbType;
   }>()
 );
+
 export const editUserSuccess = createAction(
-  '[Users Detail] Edit User Success', props<{ userData: UsersDTO }>());
+  '[Users Detail] Edit User Success',
+  props<{ userData: UsersDTO }>()
+);
+
 export const editUserFailed = createAction(
-  '[Users Detail] Edit Failed', props<{ error: UsersErrors | null }>());
+  '[Users Detail] Edit Failed',
+  props<{ error: UsersErrors | null }>()
+);
 
 export const loadUser = createAction(
   '[Users Page] Load User');
 export const loadUserSuccess = createAction(
-  '[Users/Api] Load User Success', props<{ userData: UsersEntity }>());
+  '[Users/Api] Load User Success',
+  props<{ userData: UsersEntity }>()
+);
 export const loadUserFailed = createAction(
-  '[Users/Api] Load User Failed', props<{ error: any }>());
+  '[Users/Api] Load User Failed',
+  props<{ error: any }>()
+);
 
 export const updateUserStatus = createAction(
-  '[Users Detail] Update User Status', props<{ status: LoadingStatus }>());
+  '[Users Detail] Update User Status',
+  props<{ status: LoadingStatus }>()
+);
 
 export const setUsersFilter = createAction(
-  '[Users] Set Users Filter', props<{ filter: { name: string } }>());
+  '[Users] Set Users Filter',
+  props<{ filter: { name: string } }>()
+);
--- libs/users/users/data-access/src/lib/+state/users.selectors.ts
+++ libs/users/users/data-access/src/lib/+state/users.selectors.ts
@@ -3,47 +3,45 @@ import { USERS_FEATURE_KEY, UsersState, usersAdapter } from './users.reducer';
 import { selectRouteParams } from '@users/core/data-access';
 
 // Lookup the 'Users' feature state managed by NgRx
-export const selectUsersState =
-  createFeatureSelector<UsersState>(USERS_FEATURE_KEY);
+export const selectUsersState = createFeatureSelector<UsersState>(USERS_FEATURE_KEY);
 
 const { selectAll, selectEntities } = usersAdapter.getSelectors();
 
-export const selectUsersStatus =
-  createSelector(selectUsersState, (state: UsersState) => state.status);
+export const selectUsersStatus = createSelector(
+  selectUsersState, (state: UsersState) => state.status);
 
-export const selectUsersError =
-  createSelector(selectUsersState, (state: UsersState) => state.error);
+export const selectUsersError = createSelector(
+  selectUsersState, (state: UsersState) => state.error);
 
-export const selectAllUsers =
-  createSelector(selectUsersState, (state: UsersState) => selectAll(state));
+export const selectAllUsers = createSelector(
+  selectUsersState, (state: UsersState) => selectAll(state));
 
-export const selectUsersEntities =
-  createSelector(selectUsersState, (state: UsersState) => selectEntities(state));
+export const selectUsersEntities = createSelector(
+  selectUsersState, (state: UsersState) => selectEntities(state));
 
-export const selectSelectedId =
-  createSelector(selectUsersState, (state: UsersState) => state.selectedId);
+export const selectSelectedId = createSelector(
+  selectUsersState, (state: UsersState) => state.selectedId);
 
-export const selectEntity =
-  createSelector(selectUsersEntities, selectSelectedId, (entities, selectedId) =>
+export const selectEntity = createSelector(
+  selectUsersEntities, selectSelectedId, (entities, selectedId) =>
     selectedId ? entities[selectedId] : undefined
   );
 
-export const selectUserById = (id: number) =>
-  createSelector(selectUsersEntities, (entities) =>
+export const selectUserById = (id: number) => createSelector(
+  selectUsersEntities, (entities) =>
     entities[id]);
 
-export const selectOpenedUser =
-  createSelector(
-    selectRouteParams,
-    selectUsersEntities,
+export const selectOpenedUser = createSelector(
+  selectRouteParams,
+  selectUsersEntities,
     ({ id }, entities) => entities[id] || null
   );
 
-export const usersFilterSelector =
-  createSelector(selectUsersState, (state: UsersState) => state.usersFilter);
+export const usersFilterSelector = createSelector(
+  selectUsersState, (state: UsersState) => state.usersFilter);
 
-export const filteredUsersSelector =
-  createSelector(selectAllUsers, usersFilterSelector, ((users, usersFilter) =>
+export const filteredUsersSelector = createSelector(
+  selectAllUsers, usersFilterSelector, ((users, usersFilter) =>
       usersFilter.name ? users.filter(user =>
         user.name.toLowerCase().includes(usersFilter.name.toLowerCase())) : users
   ));
