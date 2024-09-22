e157ea41f8481261c955ee1eac411f0937727fac
Timur Ayupov <t.r.ayupov@gmail.com>
2024-09-22 10:57:39 +0500

    chore(materials) add html

--- libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-list/folders-list.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoldersListViewModel } from './folders-list-view-model';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { UsersCardComponent } from '../../../../../users/feature-users-list/src/lib/users-card/users-card.component';
 
 @Component({
   selector: 'users-folders-list',
   standalone: true,
-  imports: [CommonModule, MatProgressBarModule],
+  imports: [CommonModule, MatProgressBarModule, UsersCardComponent],
   templateUrl: './folders-list.component.html',
   styleUrls: ['./folders-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
