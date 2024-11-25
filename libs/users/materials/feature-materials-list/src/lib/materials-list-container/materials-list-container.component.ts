bf79c347d295c6e1861add3522cae3c0c7b25df0
Timur Ayupov <t.r.ayupov@gmail.com>
2024-11-25 18:28:05 +0500

    chore(materials-list-container) details added

--- libs/users/materials/feature-materials-list/src/lib/materials-list-container/materials-list-container.component.ts
+++ libs/users/materials/feature-materials-list/src/lib/materials-list-container/materials-list-container.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateMaterialsButtonComponent } from '@users/materials/feature-materials-create';
 
 @Component({
   selector: 'users-materials-list-container',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CreateMaterialsButtonComponent],
   templateUrl: './materials-list-container.component.html',
   styleUrls: ['./materials-list-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
