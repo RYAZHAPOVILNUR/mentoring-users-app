ffba64344a4bab07478d196f615edfe39e95e9bf
FayaAkh <150663753+FayaAkh@users.noreply.github.com>
2024-11-28 16:40:33 +0200

    create/add users-filter-component

---
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'users-filter',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './users-filter.component.html',
+    styleUrls: ['./users-filter.component.scss'],
+  })
+
+  export class UsersFilterComponent {
+  
+  }
+  
