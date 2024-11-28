db2fc5c5ec1a2f1b40d8653e3287ab2d6ce0555e
FayaAkh <150663753+FayaAkh@users.noreply.github.com>
2024-11-28 18:00:29 +0200

    add reactiveForm

--- libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -1,15 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'users-filter',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './users-filter.component.html',
     styleUrls: ['./users-filter.component.scss'],
   })
 
-  export class UsersFilterComponent {
-  
+  export class UsersFilterComponent implements OnInit {
+    form = new FormGroup({nameValue: new FormControl('')});
+    ngOnInit(): void {
+      console.log(this.form.value)
+    }
+
+    onHandle(){
+      console.log(this.form.value)
+     
+    }
   }
   
