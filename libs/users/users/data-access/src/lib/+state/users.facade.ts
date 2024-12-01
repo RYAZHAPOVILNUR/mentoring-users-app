8d12eb54bbf142b1187fe671ef1019b61f64a0fc
Proxima <nanamofeevn@gmail.com>
2024-12-01 18:24:42 +0300

    feat: add user filter

--- libs/users/users/data-access/src/lib/+state/users.facade.ts
+++ libs/users/users/data-access/src/lib/+state/users.facade.ts
@@ -21,7 +21,10 @@ export class UsersFacade {
   public readonly selectedUsers$ = this.store.pipe(select(UsersSelectors.selectEntity));
   public readonly openedUser$ = this.store.select(UsersSelectors.selectOpenedUser);
   public readonly loggedUser$ = this.store.select(selectLoggedUser);
+  public readonly filteredUsers$ = this.store.select(UsersSelectors.filteredUsers);
   public readonly errors$: Observable<UsersErrors | null> = this.store.pipe(select(UsersSelectors.selectUsersError));
+  
+
   /**
    * Use the initialization action to perform one
    * or more tasks in your Effects.
@@ -57,4 +60,8 @@ export class UsersFacade {
   loadUser() {
     this.store.dispatch(UsersActions.loadUser());
   }
+
+  filterUsers(name: string) {
+   this.store.dispatch(UsersActions.setUsersFilter({name}));
+ }
 }
--- libs/users/users/data-access/src/lib/+state/users.selectors.ts
+++ libs/users/users/data-access/src/lib/+state/users.selectors.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { USERS_FEATURE_KEY, UsersState, usersAdapter } from './users.reducer';
-import { selectQueryParam, selectQueryParams, selectRouteParams } from '@users/core/data-access';
+import { selectRouteParams } from '@users/core/data-access';
 
 // Lookup the 'Users' feature state managed by NgRx
 export const selectUsersState = createFeatureSelector<UsersState>(USERS_FEATURE_KEY);
@@ -20,6 +20,14 @@ export const selectSelectedId = createSelector(selectUsersState, (state: UsersSt
 export const selectEntity = createSelector(selectUsersEntities, selectSelectedId, (entities, selectedId) =>
   selectedId ? entities[selectedId] : undefined
 );
+
+export const usersFilterSelector = createSelector(selectUsersState, (state: UsersState) => state.filter);
+
+export const filteredUsers = createSelector(
+ selectAllUsers,
+ usersFilterSelector,
+ (entities, filterUsers) => entities.filter(user => user.name.toLowerCase().includes(filterUsers.name.toLowerCase()))
+)
 
 export const selectUserById = (id: number) => createSelector(selectUsersEntities, (entities) => entities[id]);
 
