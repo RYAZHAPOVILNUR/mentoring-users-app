a5287a966192f6ce6ec7af6ba7c808c113982976
Yana <yana@example.com>
2024-12-29 20:54:32 +0300

    refactor: typed error

--- libs/users/materials/data-access/src/lib/+state-folders/folders.actions.ts
+++ libs/users/materials/data-access/src/lib/+state-folders/folders.actions.ts
@@ -8,11 +8,11 @@ export interface AppError {
 
 export const initFolders = createAction('[Folders Page] Init')
 export const loadFoldersSuccess = createAction('[Folders/API] Load Folders Success', props<{ folders: FoldersType[] }>());
-export const loadFoldersFailure = createAction('[Folders/API] Load Folders Failure', props<{ error: any }>());
+export const loadFoldersFailure = createAction('[Folders/API] Load Folders Failure', props<{ error: AppError }>());
 
 export const deleteFolder = createAction('[Folders Page] Delete Folder', props<{ id: number }>());
 export const deleteFolderSuccess = createAction('[Folders/Api] Delete Folder Success', props<{ id: number }>());
-export const deleteFolderFailed = createAction('[Folders/Api] Delete Folder Failed', props<{ error: any }>());
+export const deleteFolderFailed = createAction('[Folders/Api] Delete Folder Failed', props<{ error: AppError }>());
 
 export const addFolder = createAction('[Folders Page] Add Folders', props< AddFoldersType >());
 export const addFolderSuccess = createAction('[Folders/Api] Add Folders Success', props<{ folder: FoldersType }>());
