f17206cceb910045dbb8d94f1599d2e90ad349f9
Daniil0684 <sivakmisvak@gmail.com>
2025-01-01 13:02:29 +0500

    [#3] (refactor) some changes in code lines in users-filter.component.ts

--- libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Store } from '@ngrx/store';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { UsersFacade } from '@users/users/data-access';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 
 @Component({
@@ -17,9 +16,8 @@ import { UsersFacade } from '@users/users/data-access';
 })
 export class UsersFilterComponent implements OnInit {
   private readonly usersFacade = inject(UsersFacade)
-  private readonly store = inject(Store)
-  usersFilter = new FormControl('');
   private readonly destroyRef = inject(DestroyRef);
+  usersFilter = new FormControl('');
 
   ngOnInit() {
     this.usersFilter.valueChanges
