b41ee2b0a31a17396add1440cbdc57859c9e6112
AbuAbdulmalik <mr.betterdoctor@gmail.com>
2025-01-26 09:53:31 +0300

    refactor: Added unsubscribe using takeUtilDestroyed in the component users-filter.component.ts and created action setUsersFilter

--- libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, EventEmitter, inject, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'users-users-filter',
@@ -20,7 +21,9 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
   standalone: true
 })
 export class UsersFilterComponent {
+  private readonly destroyRef = inject(DestroyRef);
   public readonly searchUsers = new FormControl('');
+
   @Output() filterUsers = new EventEmitter<string>();
 
   constructor() {
@@ -28,6 +31,7 @@ export class UsersFilterComponent {
       .pipe(
         debounceTime(300),
         distinctUntilChanged(),
+        takeUntilDestroyed(this.destroyRef),
         )
       .subscribe((value) => this.filterUsers.emit(value ?? ''.trim()))
   }
