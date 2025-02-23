7b62b6e7c391f98a4c522d190f7b298b3e318a71
AbuAbdulmalik <mr.betterdoctor@gmail.com>
2025-02-23 11:39:41 +0300

    feat(materials): add library data-access

--- libs/users/materials/data-access/src/lib/+state/materials.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromMaterials from './materials.reducer';
-
-export const selectMaterialsState = createFeatureSelector<fromMaterials.State>(
-  fromMaterials.materialsFeatureKey
-);
