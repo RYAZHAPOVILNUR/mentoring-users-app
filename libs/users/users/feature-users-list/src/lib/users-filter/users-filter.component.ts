273e332bcb40b4a7f1872dd1252f926a86f8ce72
Rubsee <r.u.s.i.k.99@mail.ru>
2025-03-02 17:13:56 +0300

    [fix] added unsub in users-filter component

--- libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UsersFacade } from '@users/users/data-access';
-import { debounceTime, startWith, Subject } from 'rxjs';
+import { debounceTime, startWith, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'users-filter',
@@ -11,17 +11,23 @@ import { debounceTime, startWith, Subject } from 'rxjs';
   templateUrl: './users-filter.component.html',
   styleUrls: ['./users-filter.component.scss'],
 })
-export class UsersFilterComponent implements OnInit {
+export class UsersFilterComponent implements OnInit, OnDestroy {
   private readonly usersFacade = inject(UsersFacade);
   private readonly inputSubject = new Subject<string>();
+  private readonly destroy$ = new Subject<void>();
 
   ngOnInit(): void {
-    this.inputSubject.pipe(startWith(''), debounceTime(300)).subscribe((inputText) => {
+    this.inputSubject.pipe(startWith(''), debounceTime(300), takeUntil(this.destroy$)).subscribe((inputText) => {
       this.usersFacade.setFilterUsers(inputText || '');
     });
   }
 
   onInputChange(inputText: string): void {
     this.inputSubject.next(inputText);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
