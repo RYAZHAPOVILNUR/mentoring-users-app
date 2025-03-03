ed494fb3ebf64c51f6a02421b612d1a569146598
AbuAbdulmalik <mr.betterdoctor@gmail.com>
2025-03-03 15:42:58 +0300

    feat(materials): add folders-list-container.store.ts

--- libs/users/materials/data-access/src/index.ts
+++ libs/users/materials/data-access/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/+state/folders/folders.actions';
 export * from './lib/+state/folders/folders.reducer';
 export * from './lib/+state/folders/folders.selectors';
 export * from './lib/+state/folders/folders.effects';
+export * from './lib/+state/folders/folders.facade';
---
+++ libs/users/materials/feature-folders-list/src/lib/folders-list-container/folders-list-container.store.ts
@@ -0,0 +1,68 @@
+import { inject, Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { tap } from 'rxjs';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+import { DeepReadonly } from '@users/core/utils';
+import { CoreUiConfirmDialogComponent } from '@users/core/ui';
+import { FoldersEntity } from '@users/core/data-access';
+import { foldersVmAdapter } from '../../../../folders-vm.adapter';
+import { FoldersVm } from '../../../../folders-vm';
+import { FoldersFacade } from '@users/materials/data-access';
+
+export type FoldersListState = DeepReadonly<{
+  folders: FoldersVm[]
+}>
+
+const initialState: FoldersListState = {
+  folders: []
+};
+
+@Injectable()
+export class FoldersListContainerStore extends ComponentStore<FoldersListState> {
+  public readonly folders$ = this.select(({ folders }) => folders);
+  private readonly foldersFacade = inject(FoldersFacade);
+  public readonly status$ = this.select(
+    this.foldersFacade.status$,
+    (status) => status
+  );
+  public errors$ = this.select(this.foldersFacade.errors$, (error) => error);
+  private readonly dialog = inject(MatDialog);
+
+  constructor() {
+    super(initialState);
+    this.foldersFacade.init();
+    this.setFoldersFromGlobalToLocalStore();
+  }
+
+  public deleteFolder(folder: FoldersVm): void {
+    const dialogRef: MatDialogRef<CoreUiConfirmDialogComponent> = this.dialog.open(CoreUiConfirmDialogComponent, {
+      data: { dialogText: `Вы уверены, что хотите удалить ${folder.title}` }
+    });
+    this.effect(
+      () => dialogRef.afterClosed().pipe(
+        tap(
+          (result: boolean) => {
+            if (result) this.foldersFacade.deleteFolder(folder.id);
+          }
+        )
+      )
+    );
+  }
+
+  private setFoldersFromGlobalToLocalStore(): void {
+    this.effect(
+      () => this.foldersFacade.allFolders$.pipe(
+        tap((folders: FoldersEntity[]) => this.patchUsers(folders))
+      )
+    );
+  }
+
+  private patchUsers(folders: FoldersEntity[]): void {
+    this.patchState({
+      folders: folders.map(
+        folder => foldersVmAdapter.entityToVm(folder)
+      )
+    });
+  }
+}
