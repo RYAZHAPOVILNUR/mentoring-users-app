4d8e34d3afffd101aeaf15cd716ee447ac01d947
Rinat Fattakhov <ashtn17@gmail.com>
2025-03-13 18:44:13 +0500

    [#3] feat: Add users filter

---
+++ libs/users/users/feature-users-list/src/lib/users-filter/users-filter.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, ViewEncapsulation, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsersFacade } from '@users/users/data-access';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'users-filter',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  templateUrl: './users-filter.component.html',
+  styleUrls: ['./users-filter.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersFilterComponent implements OnDestroy {
+  private readonly usersFacade = inject(UsersFacade);
+  private destroy$ = new Subject<void>();
+
+  nameFilter = new FormControl('');
+
+  constructor() {
+    this.nameFilter.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((value) => {
+        this.usersFacade.setFilter(value || '');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
