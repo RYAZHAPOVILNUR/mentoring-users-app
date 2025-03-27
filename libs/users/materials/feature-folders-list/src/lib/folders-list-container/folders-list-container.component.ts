cb06b5d6364bcc317fba946c8f584367e63e3c7d
щахрух <132030072+shepherd1422@users.noreply.github.com>
2025-03-27 17:37:39 +0900

    folders-section is working)

--- libs/users/materials/feature-folders-list/src/lib/folders-list-container/folders-list-container.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/folders-list-container/folders-list-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateUsersButtonComponent, UsersVM } from '@users/feature-users-create';
 import { LetDirective } from '@ngrx/component';
@@ -8,6 +8,9 @@ import { FoldersVM } from '../../../../folders-vm';
 import {
   FoldersAddButtonComponent
 } from '../../../../feature-folders-create/folders-add-button/folders-add-button.component';
+import {
+  UsersListContainerStore
+} from '../../../../../users/feature-users-list/src/lib/users-list-container/users-list-container.store';
 
 @Component({
   selector: 'users-folders-list-container',
@@ -16,6 +19,9 @@ import {
   templateUrl: './folders-list-container.component.html',
   styleUrls: ['./folders-list-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.Emulated,
+  providers: [FoldersListContainerStore],
+
 })
 export class FoldersListContainerComponent {
   private readonly componentStore = inject(FoldersListContainerStore);
