d7bb215db1b7194bfa71c622eb58218387940c41
Nail Khabibullin <cznailmail@gmail.com>
2025-04-03 17:51:38 +0400

    feat(materials) add component exports in index.ts for feature-folders-list library

--- libs/users/materials/feature-folders-list/src/index.ts
+++ libs/users/materials/feature-folders-list/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/feature-folders-list/feature-folders-list.component';
+export * from './lib/feature-folders-list/folders-card/folders-card.component';
+export * from './lib/feature-folders-list/folders-list/folders-list.component';
+export * from './lib/feature-folders-list/folders-list-container/folders-list-container.component';
