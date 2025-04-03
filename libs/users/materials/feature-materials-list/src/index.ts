deb31a08fb07a442d0fc516488575b3863b1e6d3
Nail Khabibullin <cznailmail@gmail.com>
2025-04-03 18:21:42 +0400

    feat(materials) generate feature-materials-list library & materials-list-container, materials-list, materials-card components

---
+++ libs/users/materials/feature-materials-list/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/feature-materials-list/feature-materials-list.component';
+export * from './lib/feature-materials-list/materials-list/materials-list.component';
+export * from './lib/feature-materials-list/materials-list-container/materials-list-container.component';
+export * from './lib/feature-materials-list/materials-card/materials-card.component';
