6e6c33989f7ce6891aaf415714c3e62bf5b5f17d
Nail Khabibullin <cznailmail@gmail.com>
2025-04-04 19:06:37 +0400

    feat(materials) fixed and include folder-list.component at feature-folders-list library logic

--- libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-list-container/folders-list-container.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-list-container/folders-list-container.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FoldersListContainerStore } from './folders-list-container.store';
 import { FoldersCardComponent } from '../folders-card/folders-card.component';
 import { NgFor, NgIf, AsyncPipe } from '@angular/common';
+import { FoldersListComponent } from "../folders-list/folders-list.component";
+import { map } from 'rxjs';
 
 @Component({
   selector: 'users-folders-list-container',
@@ -12,7 +14,9 @@ import { NgFor, NgIf, AsyncPipe } from '@angular/common';
     FoldersCardComponent,
     NgFor,
     NgIf,
-    AsyncPipe],
+    AsyncPipe,
+    FoldersListComponent
+],
   templateUrl: './folders-list-container.component.html',
   styleUrls: ['./folders-list-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,7 +24,9 @@ import { NgFor, NgIf, AsyncPipe } from '@angular/common';
 })
 export class FoldersListContainerComponent {
  private readonly componentStore = inject(FoldersListContainerStore)
-  public readonly folders$ = this.componentStore.folders$
+ public readonly folders$ = this.componentStore.folders$.pipe(
+  map(folders => folders ?? [])
+);
   
   constructor() {
     this.componentStore.loadFolders();
--- libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-list/folders-list.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-list/folders-list.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FoldersCardComponent } from "../folders-card/folders-card.component";
+import { Folder } from '@users/materials/data-access';
 
 @Component({
   selector: 'users-folders-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FoldersCardComponent],
   templateUrl: './folders-list.component.html',
   styleUrls: ['./folders-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FoldersListComponent {}
+export class FoldersListComponent {
+  @Input() folders: Folder[] = [];
+}
