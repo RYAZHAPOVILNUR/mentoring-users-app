7ea03cb8bbca0ec13fb7944aa4204e0b4e171644
Nail Khabibullin <cznailmail@gmail.com>
2025-04-04 17:42:44 +0400

    feat(materials) added styles for folder components and dependences

--- libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-card/folders-card.component.ts
+++ libs/users/materials/feature-folders-list/src/lib/feature-folders-list/folders-card/folders-card.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Folder } from '@users/materials/data-access';
+import { FeatureFoldersListComponent } from "../feature-folders-list.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'users-folders-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FeatureFoldersListComponent,
+    MatIconModule
+  ],
   templateUrl: './folders-card.component.html',
   styleUrls: ['./folders-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
