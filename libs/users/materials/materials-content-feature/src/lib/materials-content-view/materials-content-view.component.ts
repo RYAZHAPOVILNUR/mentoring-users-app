88fddff50d2b8d70214741edf07e1fbe3608dee3
NolOdin <mr.ciborg@outlook.com>
2025-06-10 14:44:25 +0300

    created materials youtube player feature

--- libs/users/materials/materials-content-feature/src/lib/materials-content-view/materials-content-view.component.ts
+++ libs/users/materials/materials-content-feature/src/lib/materials-content-view/materials-content-view.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PDFProgressData, PdfViewerModule } from 'ng2-pdf-viewer';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {YoutubePlayerComponent} from '@youtube-player'
+//import { YoutubePlayerComponent_1 as YoutubePlayerComponent } from "../../../../../../core/youtube-player/src/lib/youtube-player/youtube-player.component";
 
 @Component({
   selector: 'lib-materials-content-view',
-  imports: [CommonModule, CommonModule, PdfViewerModule, MatProgressBarModule],
+  imports: [CommonModule, CommonModule, PdfViewerModule, MatProgressBarModule, YoutubePlayerComponent],
   templateUrl: './materials-content-view.component.html',
   styleUrl: './materials-content-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,6 +19,8 @@ export class MaterialsContentViewComponent {
   public loadedProgress = true;
   public audioLoaded = false;
   public imageLoaded = false;
+  public videoLoaded = false
+  //public hereVideoId = this.getVideoId()
 
   onProgress(progressData: PDFProgressData) {
     if (progressData.total === progressData.loaded) {
@@ -30,18 +34,23 @@ export class MaterialsContentViewComponent {
     }
   }
 
+  onVideoLoaded(isReady: boolean) {
+    console.log(isReady)
+    if(isReady == true) {
+      this.loadedProgress = false
+    }
+  }
+
   onImageLoaded() {
     if (this.imageLoaded != true) {
       this.loadedProgress = false;
     }
   }
 
-  public getVideoId(): string | null {
+  public getVideoId(): string {
     const youtubeRegex = /[?&]v=([a-zA-Z0-9_-]{11})|\/embed\/([a-zA-Z0-9_-]{11})/;
     const match = this.materialLink.match(youtubeRegex);
-    return match ? match[1] || match[2] : null;
+    return match ? match[1] : ""
   }
-  constructor() {
-    console.log(this.data);
-  }
+
 }
