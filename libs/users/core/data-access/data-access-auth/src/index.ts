674de5ac6b72878988270b9ad4009f23d64302aa
sabae <Sabaev-nik29@yandex.ru>
2025-07-27 12:19:00 +0300

    refactor(ngrx store): deleted ngrx store

--- libs/users/core/data-access/data-access-auth/src/index.ts
+++ libs/users/core/data-access/data-access-auth/src/index.ts
@@ -1,10 +1,5 @@
-export * as authSelectors from './lib/+state/auth.selectors';
-export * as authEffects from './lib/+state/auth.effects';
-export { authActions } from './lib/+state/auth.actions';
-export { authFeature } from './lib/+state/auth.reducer';
-export { AuthFacade } from './lib/auth.facade';
-export { adminGuard } from './lib/guards/admin.guard';
-export { authGuard } from './lib/guards/auth.guard';
 export { AuthStore } from './lib/+state/auth-signal-store';
-// todo
-export * from './lib/+state/sign.auth.model';
+export { AuthFacade } from './lib/auth.facade';
+
+export { authGuard } from './lib/guards/auth.guard';
+export { adminGuard } from './lib/guards/admin.guard';
