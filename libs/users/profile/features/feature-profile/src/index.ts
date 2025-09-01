b525f2585f0d97139a00f7f32ceb1bbd014fd715
Arlan <arlan.amirov.2015@mail.ru>
2025-09-02 00:33:53 +0500

    feat [02.09.2025]: timer task is done

--- libs/users/profile/features/feature-profile/src/index.ts
+++ libs/users/profile/features/feature-profile/src/index.ts
@@ -1,2 +1,3 @@
 export { UserProfileContainerComponent } from './lib/pages/user-profile-container/user-profile-container.component';
 export { SelfProfileContainerComponent } from './lib/pages/self-profile-container/self-profile-container.component';
+export { TimerService, timerValue } from './lib/services/timer.service';
