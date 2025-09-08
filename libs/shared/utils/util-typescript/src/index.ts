b683941a46561495bf501acd66ce2374bafa6f7b
Evgeniy <bazhin_ea@taximaxim.ru>
2025-09-08 17:17:26 +0300

    chore(shared): Added utility Form type & nullable properties

--- libs/shared/utils/util-typescript/src/index.ts
+++ libs/shared/utils/util-typescript/src/index.ts
@@ -1,3 +1,6 @@
 export { DeepReadonly } from './lib/deep-readonly';
 export { Callback } from './lib/callback.type';
 export { ObjectValues } from './lib/object-values.type';
+export { FormType } from './lib/form-type.type';
+export { NullableProperties } from './lib/nullable-properties.type';
+export { NonNullableProperties } from './lib/non-nullable-properties.type';
